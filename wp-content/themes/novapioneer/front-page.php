f7bbcea50a843e8f26e6e0bf910650a37994f086
Schambach Milimu <schambach@circle.co.ke>
2016-11-16 11:39:50 +0300

    A tiny change.

--- wp-content/themes/novapioneer/front-page.php
+++ wp-content/themes/novapioneer/front-page.php
@@ -16,7 +16,9 @@
             <article>
                 <i>
                     <blockquote cite="<?php echo $testimonial->post_title; ?>">
-                        <strong><?php echo $testimonial->post_content; ?></strong>
+                        <strong><?php echo $testimonial->post_content; ?></strong><br/>
+                        <span <style="display:block; width: 100px; border-top: solid 1px #777;"></span>
+                        <span><?php echo $testimonial->post_title; ?> </span>
                     </blockquote>
                 </i>
             </article>
