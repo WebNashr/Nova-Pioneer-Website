703a8a2acfd879598726af049e3f5e91511d8cbd
Schambach Milimu <schambach@circle.co.ke>
2016-11-18 17:40:50 +0300

    Tweak on about page.

--- wp-content/themes/novapioneer/about-page.php
+++ wp-content/themes/novapioneer/about-page.php
@@ -12,6 +12,7 @@ get_header();?>
         $our_essence = get_field('our_essence');
         $our_culture = get_field('our_culture');
         $our_brand_message = get_field('our_brand_message');
+        $members = get_field('leadership_team_members');
     
     ?>
 
@@ -36,39 +37,18 @@ get_header();?>
                 <?php echo $our_brand_message; ?>
             </section>
 
-            <?php $leadership_team_query = new WP_Query(array(
-                "post_type" => "team",
-                "orderby" => "menu_order",
-                "order" => "ASC",
-                "posts_per_page" => -1,
-                "tax_query" => array(
-                    array(
-                        "taxonomy" => "team_member_category",
-                        "field"    => "slug",
-                        "terms"    => "leadership-team"
-                    )
-                )
-            )); ?>
-
-            <?php if( $leadership_team_query->have_posts() ): ?>
-
-            <div>
-                <h1>Leadership Team</h1>
-                <?php while( $leadership_team_query->have_posts() ): $leadership_team_query->the_post(); ?>
-
-                    <article>
-                        <header>
-                            <h1> <a href="<?php echo get_permalink(); ?>"> <?php echo get_the_title(); ?> </a> </h1>
-                            <p><?php echo get_field('title'); ?></p>
-                            <?php if(has_post_thumbnail()): the_post_thumbnail(); endif;?>
-                        </header>                        
-                    </article>
-
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            </div>
-
-            <?php endif; ?>
+        <?php foreach($members as $member): $member = (object)$member; ?>
+            <section>
+                <header>
+                    <h1> <a href="<?php echo get_permalink($member->ID); ?>"> <?php echo $member->post_title; ?> </a> </h1>
+                    <p><?php echo get_field('title'); ?></p>
+                    <?php $headshot_url = wp_get_attachment_image_src(get_post_thumbnail_id($member->ID, 'single-post-thumbnail')); ?>
+                    <?php if(!empty($headshot_url)): ?>
+                        <img src="<?php echo $headshot_url; ?>" />
+                    <?php endif; ?>
+                </header>                        
+            </section>
+        <?php endforeach; ?>
 
 
         </article>
