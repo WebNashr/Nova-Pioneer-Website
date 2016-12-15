9e811815574f270bfdf4b87a7dd0e07e928adde6
Schambach Milimu <schambach@circle.co.ke>
2016-12-15 14:02:10 +0300

    Changes on corporate page

--- wp-content/themes/novapioneer/corporate-page.php
+++ wp-content/themes/novapioneer/corporate-page.php
@@ -16,26 +16,49 @@ get_header();?>
     
     ?>
 
-    <article>
-        <header>
-            <h1><?php echo get_the_title(); ?></h1>
-        </header>
 
-        <section>
-            <h2>School Mission</h2>
-            <?php echo $school_mission; ?>
+
+        <section class="section section-hero corporate-hero">
+            <div class="container hero-container">
+                <div class="main-callout-box">
+                    <hr>
+                    <h1><?php the_title(); ?></h1>
+                </div>
+            </div>
         </section>
 
-        <section>
-            <h2>School Vision</h2>
-            <?php echo $school_vision; ?>
+        <div class="trigger"></div>
+        <section class="section">
+            <article class="article">
+                <header class="article-header">
+                    <h1>Corporate Section</h1>
+                </header>
+
+                <div class="article-container">
+                    <aside class="article-aside">
+                        <div class="downloads">
+                            <p>
+                                Our Annual Reports give a concise overview of our operations in a given year. Download them as a PDF document below:
+                            </p>
+                        </div>
+                        <?php foreach( get_field('annual_reports') as $report ): $report = (object)$report; ?>
+                        <div class="downloads">
+                            <h4><?php echo $report->title; ?></h4>
+                            <a download="<?php echo $report->title; ?>" href="<?php echo $report->file; ?>" class="button button-tiny button-secondary">Download PDF</a>
+                        </div>
+                        <?php endforeach; ?>
+                    </aside>
+
+                    <div class="article-inner">
+                        <p class="article-excerpt"><?php echo get_field('intro'); ?></p>
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </article>
         </section>
 
-        <section>
-            <h2>Company Structure</h2>
-            <?php echo $company_structure; ?>
-        </section>
-    </article>
+
+
 
     <?php endwhile; ?>
 
