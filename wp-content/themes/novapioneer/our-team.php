06fc9db4d54f8e2531e6a9053bf9183f35269ed8
edgar chris <edgarchris4@gmail.com>
2016-12-23 12:40:02 +0300

    fix toggle

--- wp-content/themes/novapioneer/our-team.php
+++ wp-content/themes/novapioneer/our-team.php
@@ -43,13 +43,13 @@ get_header();?>
 
             <div class="section-content section-content-plain">
 
-                <?php foreach( get_field('team_members') as $member): $member = (object)$member; ?>
+                <?php $i=1; foreach( get_field('team_members') as $member): $member = (object)$member; ?>
                     <div class="section-content-item section-content-item-quarter profile">
                         <img src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $member->ID ), 'single-post-thumbnail' )[0]; ?>" alt="<?php echo $member->post_title; ?>">
                         <h1 class="profile-name"><?php echo $member->post_title; ?></h1>
                         <h2 class="profile-role"><?php echo get_field('title', $member->ID); ?></h2>
                         <div class="profile-description">
-                            <input type="checkbox" class="read-more-state" id="post-1" />
+                            <input type="checkbox" class="read-more-state" id="post-<?php echo $i?>" />
                             <?php
                                 // Get the paragraphs in the post content for implementing the read-more functionality
                                 preg_match_all('|<p>(.+?)</p>|', $member->post_content, $matches);
@@ -63,10 +63,11 @@ get_header();?>
                                     <?php endforeach; ?>
                                 </span> 
                             </div>
-                            <label for="post-1" class="read-more-trigger button button-tiny button-primary"></label>
+                            <label for="post-<?php echo $i?>" class="read-more-trigger button button-tiny button-primary"></label>
                         </div>
                     </div>
-                <?php endforeach; ?>
+
+                <?php $i++ ; endforeach; ?>
 
             </div>
         </section>
