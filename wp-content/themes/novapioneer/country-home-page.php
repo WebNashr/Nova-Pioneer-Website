b0515dca6c3083803e710237c20498767a9df0db
edgar chris <edgarchris4@gmail.com>
2017-01-04 12:49:42 +0300

    thumbnail

--- wp-content/themes/novapioneer/country-home-page.php
+++ wp-content/themes/novapioneer/country-home-page.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <?php if(have_posts()): ?>
 
     <?php while(have_posts()): the_post(); ?>
-    
+
         <section class="section section-hero country-home">
             <div class="container hero-container">
                 <!-- <heading class="hero-heading">
@@ -29,7 +29,7 @@ get_header(); ?>
             <?php $schools = get_field('schools'); ?>
             <?php foreach($schools as $school): ?>
                 <div class="section-school-list-select">
-                    <p class="school-photo"><img src="<?php echo get_template_directory_uri(); ?>/img/ormonde-school.jpg" alt=""></p>
+                    <p class="school-photo"><img src="<?php echo get_the_post_thumbnail_url($school->ID,'thumbnail'); ?>" alt=""></p>
                     <h1><a href="<?php echo get_permalink($school->ID); ?>" class="school-link" title=""><?php echo $school->post_title; ?></a></h1>
                     <p class="school-summary"><?php echo get_field('summary', $school->ID); ?></p>
                 </div>
@@ -50,7 +50,7 @@ get_header(); ?>
                         <div class="testimonial pull-quote">
                             <?php
                                 $our_students_video             = get_field('our_students_video');
-                                $vid_caption = get_field('video_caption', $our_students_video->ID); 
+                                $vid_caption = get_field('video_caption', $our_students_video->ID);
                                 $student_name = get_field('student_name', $our_students_video->ID);
                                 $video = get_field('video', $our_students_video->ID);
                             ?>
@@ -84,8 +84,8 @@ get_header(); ?>
                 <div class="section-content-item section-content-item-half">
                     <div class="testimonial pull-quote">
                         <?php
-                            $learning_at_novapioneer_video  = get_field('learning_at_novapioneer_video'); 
-                            $vid_caption = get_field('video_caption', $learning_at_novapioneer_video->ID); 
+                            $learning_at_novapioneer_video  = get_field('learning_at_novapioneer_video');
+                            $vid_caption = get_field('video_caption', $learning_at_novapioneer_video->ID);
                             $caption_speaker = get_field('caption_speaker', $learning_at_novapioneer_video->ID);
                             $caption_speaker_title = get_field('caption_speaker_title', $learning_at_novapioneer_video->ID);
                             $video = get_field('video', $learning_at_novapioneer_video->ID);
@@ -121,7 +121,7 @@ get_header(); ?>
             <section class="section section-pair">
                 <?php
                     $video_from_influencer          = get_field('video_from_influencer');
-                    $vid_caption = get_field('video_caption', $video_from_influencer->ID); 
+                    $vid_caption = get_field('video_caption', $video_from_influencer->ID);
                     $caption_speaker = get_field('caption_speaker', $video_from_influencer->ID);
                     $caption_speaker_title = get_field('caption_speaker_title', $video_from_influencer->ID);
                     $video = get_field('video', $video_from_influencer->ID);
@@ -169,7 +169,7 @@ get_header(); ?>
                 <div class="general-notices-container">
                 <div class="large-notice-container">
                     <div class="large-notice">
-                        <div class="notice-content">                            
+                        <div class="notice-content">
                             <?php echo get_field('admission_call_to_action'); ?>
                             <p class="call-to-action"><a href="#" class="button button-large button-secondary" title="">Enrol Now</a></p>
                         </div>
