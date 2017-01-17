14bade2c767737bd3d37eff8553eb59842184035
Kumbi Madzinga <Kumbi Madzinga>
2017-01-17 15:19:19 +0300

    corporate page

--- wp-content/themes/novapioneer/corporate-page.php
+++ wp-content/themes/novapioneer/corporate-page.php
@@ -6,18 +6,14 @@
 
 get_header();?>
 
+
 <?php if( have_posts() ): ?>
 
     <?php while( have_posts() ): the_post();
-
         $school_mission = get_field('school_mission');
         $school_vision  = get_field('school_vision');
         $company_structure = get_field('company_structure');
-    
     ?>
-
-
-
         <section class="section section-hero corporate-hero" <?php echo set_post_new_bg()?>>
             <div class="container hero-container">
                 <div class="main-callout-box">
@@ -31,15 +27,13 @@ get_header();?>
         <section class="section">
             <article class="article">
                 <header class="article-header">
-                    <h1>Corporate Section</h1>
+                    <h1>Nova Pioneer Education Group</h1>
                 </header>
 
                 <div class="article-container">
                     <aside class="article-aside">
                         <div class="downloads">
-                            <p>
-                                Our Annual Reports give a concise overview of our operations in a given year. Download them as a PDF document below:
-                            </p>
+                            <p>Our Annual Reports give a concise overview of our operations in a given year. Download them as a PDF document below:</p>
                         </div>
                         <?php foreach( get_field('annual_reports') as $report ): $report = (object)$report; ?>
                         <div class="downloads">
@@ -56,12 +50,9 @@ get_header();?>
                 </div>
             </article>
         </section>
-
-
-
-
     <?php endwhile; ?>
 
 <?php endif; ?>
+
 
 <?php get_footer(); ?>
