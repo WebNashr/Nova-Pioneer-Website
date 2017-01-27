1d12cdce73da6b57dc717e21ba8591912c40d313
Mark <mark@circle.co.ke>
2017-01-27 14:46:38 +0300

    the new home page layout

--- wp-content/themes/novapioneer/front-page.php
+++ wp-content/themes/novapioneer/front-page.php
@@ -10,8 +10,14 @@
         <div class="main-callout-box home-page-callout">
             <hr>
             <h1><?php echo get_field('callout_box'); ?></h1>
+            <div class="country-selection">
+            <a href="#" class="button button-large button-primary" title="">Kenya</a>
+            <a href="#" class="button button-large button-primary" title="">South Africa</a>
+          </div>
         </div>
+
     </div>
+
 </section>
 <!-- <footer class="page-footer page-footer-home">
    <nav role="navigation" class="footer-menu-aux">
