d6fad0fbd0bb38d7ed521257a7c0b01ae0053c1a
Kumbi Madzinga <Kumbi Madzinga>
2017-01-30 18:00:11 +0300

    crazy calender stuff happening right here

--- wp-content/themes/novapioneer/includes/partials/content-single-event.php
+++ wp-content/themes/novapioneer/includes/partials/content-single-event.php
@@ -1,7 +1,6 @@
-<?php while( have_posts() ): the_post(); ?>
 
-
-    <section class="section section-hero events-hero">
+<?php //while( have_posts() ): the_post(); ?>
+    <!-- <section class="section section-hero events-hero">
         <div class="container hero-container">
             <div class="main-callout-box">
                 <hr>
@@ -11,21 +10,31 @@
     </section>
 
     <section class="section section-pair">
-       <div class="section-navigation">
-         <h2>Event Details</h2>
-       </div>
-        <div class="section-content">
-
-            <div class="section-content-item">
-                <div class="event-details-container">
-
-                    <?php the_content(); ?>
-                </div>
-
-            </div>
-
+        <div class="section-navigation">
+            <h2>Event Details</h2>
+            <a href="<?php echo esc_url( tribe_get_events_link() ); ?>"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'the-events-calendar' ), $events_label_plural ); ?></a>
         </div>
 
+        <div class="section-content">
+            <div class="section-content-item">
+                <div class="event-details-container">
+                    <?php //the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </section> -->
+<?php //endwhile; ?>
+
+
+
+
+
+<?php while(have_posts()): the_post(); ?>
+
+    <section class="section trigger-offset">
+        <article class="article">
+            <?php the_content(); ?>
+        </article>
     </section>
 
 <?php endwhile; ?>
