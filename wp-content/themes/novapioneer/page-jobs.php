feeb18c949b2a57987d3bd97c9b51d5e138c354a
Schambach Milimu <schambach@circle.co.ke>
2017-02-01 10:36:11 +0300

    Added jazz integration stuff.

---
+++ wp-content/themes/novapioneer/page-jobs.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<iframe name="resumator-job-frame" id="resumator-job-frame" class="resumator-advanced-widget" src="//novaacademies.applytojob.com/apply/jobs/" width="100%" scrolling="no" frameborder="0" allowTransparency="true"></iframe><script>function resizeResumatorIframe(height,nojump){if(nojump== 0){window.scrollTo(0,0);}document.getElementById("resumator-job-frame").height = parseInt(height)+20;}</script>
+
+<?php get_footer(); ?>
