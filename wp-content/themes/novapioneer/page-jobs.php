684fedb54782d06d706fffc63f70413d113e0543
Schambach Milimu <schambach@circle.co.ke>
2017-02-01 14:28:12 +0300

    a few changes on new careers page integrated with jazz

--- wp-content/themes/novapioneer/page-jobs.php
+++ wp-content/themes/novapioneer/page-jobs.php
@@ -1,5 +1,13 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * Careers Page
+ */
+get_header(); ?>
 
-<iframe name="resumator-job-frame" id="resumator-job-frame" class="resumator-advanced-widget" src="//novaacademies.applytojob.com/apply/jobs/" width="100%" scrolling="no" frameborder="0" allowTransparency="true"></iframe><script>function resizeResumatorIframe(height,nojump){if(nojump== 0){window.scrollTo(0,0);}document.getElementById("resumator-job-frame").height = parseInt(height)+20;}</script>
+<section class="section trigger-offset">
+    <article>
+        <iframe name="resumator-job-frame" id="resumator-job-frame" class="resumator-advanced-widget" src="//novaacademies.applytojob.com/apply/jobs/" width="100%" scrolling="no" frameborder="0" allowTransparency="true"></iframe><script>function resizeResumatorIframe(height,nojump){if(nojump== 0){window.scrollTo(0,0);}document.getElementById("resumator-job-frame").height = parseInt(height)+20;}</script>
+    </article>
+</section>
 
 <?php get_footer(); ?>
