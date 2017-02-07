3b96d7b437569c1dbd42935072f9701db11de565
Schambach Milimu <schambach@circle.co.ke>
2017-02-07 10:11:41 +0300

    The school gallery page should be dynamic again.

--- wp-content/themes/novapioneer/school-gallery.php
+++ wp-content/themes/novapioneer/school-gallery.php
@@ -6,22 +6,28 @@
  *
  */
 $current_gallery = $_GET['current'];
+if( empty($current_gallery) ): $current_gallery = "classrooms"; endif;
 get_header(); ?>
 
 <?php if (have_posts()): ?>
 
     <?php while (have_posts()): the_post();
         $images = novap_get_gallery_images($current_gallery); ?>
-        <?php //wp_die( var_dump( $images ) ); ?>
         <section class="section-pair section-gallery trigger-offset">
             <div class="slider-container">
                 <div class="section-navigation">
                     <h2>Gallery</h2>
                     <nav class="gallery-nav">
-                      <a href="" class="gallery-select" title="">School grounds</a>
-                      <a href="" class="gallery-select gallery-selected" title="">Classrooms</a>
-                      <a href="" class="gallery-select" title="">Library</a>
-                      <a href="" class="gallery-select" title="">Play Area</a>
+                        <a href="<?php get_permalink(); ?>?current=school_grounds"
+                           class="gallery-select <?php if ($current_gallery === 'school_grounds'): echo 'gallery-selected'; endif; ?>">School
+                            grounds</a>
+                        <a href="<?php get_permalink(); ?>?current=classrooms"
+                           class="gallery-select <?php if ($current_gallery === 'classrooms'): echo 'gallery-selected'; endif; ?>">Classrooms</a>
+                        <a href="<?php get_permalink(); ?>?current=library"
+                           class="gallery-select <?php if ($current_gallery === 'library'): echo 'gallery-selected'; endif; ?>">Library</a>
+                        <a href="<?php get_permalink(); ?>?current=play_area"
+                           class="gallery-select <?php if ($current_gallery === 'play_area'): echo 'gallery-selected'; endif; ?>">Play
+                            Area</a>
                     </nav>
                 </div>
 
@@ -29,15 +35,13 @@ get_header(); ?>
                 <div class="section-content-item-full overflow-crop">
                   <div class="media gallery">
                     <ul id="slippry">
-                      <li>
-                        <a href="#slide1"><img src="/wp-content/uploads/2016/12/slide-1.jpg" alt="this is a caption"></a>
-                      </li>
-                      <li>
-                        <a href="#slide2"><img src="/wp-content/uploads/2016/12/slide-2.jpg"  alt="another caption"></a>
-                      </li>
-                      <li>
-                        <a href="#slide3"><img src="/wp-content/uploads/2016/12/slide-3.jpg" alt="one more caption"></a>
-                      </li>
+                      <?php foreach ($images as $image): $image = (object)$image; ?>
+                        <li>
+                          <a href="#slide1">
+                            <img src="<?php echo $image->url; ?>" alt="<?php echo $image->caption; ?>">
+                          </a>
+                        </li>
+                      <?php endforeach; ?>
                     </ul>
                 </div>
             </div>
