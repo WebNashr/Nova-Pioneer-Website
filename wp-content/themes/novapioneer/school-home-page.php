22edb9fe94904d8f59f4c4f223fec41c2a599440
Mark <mark@circle.co.ke>
2017-02-20 15:13:17 +0300

    another edit to button title

--- wp-content/themes/novapioneer/school-home-page.php
+++ wp-content/themes/novapioneer/school-home-page.php
@@ -134,7 +134,7 @@ get_header(); ?>
         <section class="section section-pair">
             <div class="section-navigation">
                 <h2>Leadership Team</h2>
-                <a href="<?php echo get_permalink(); ?>our-team" class="button button-small button-primary" title="">Meet the team</a>
+                <a href="<?php echo get_permalink(); ?>our-team" class="button button-small button-primary" title="">View the team</a>
             </div>
 
             <div class="section-content">
