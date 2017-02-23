1b4aa197448421152094a50aea92af626d7e36b4
mark muema <mark@circle.co.ke>
2017-02-23 14:22:48 +0300

    applied brand colours to the principle content  on about page

--- wp-content/themes/novapioneer/about-page.php
+++ wp-content/themes/novapioneer/about-page.php
@@ -38,6 +38,49 @@ get_header();?>
 
         <section class="section">
           <div class="section-content section-content-plain principles-container">
+
+              <div class="section-content-item section-content-item-third principle-card card-1">
+                  <h2 class="number">1</h2>
+                  <h3>Joy of Learning</h3>
+                  <div class="small-divider"></div>
+                  <p>We are life-long learners and we are fueled by curiosity and discovery</p>
+              </div>
+              <div class="section-content-item section-content-item-third principle-card card-2">
+                  <h2 class="number">2</h2>
+                  <h3>Greater Together</h3>
+                  <div class="small-divider"></div>
+                  <p>We constantly support our teammates because we know that we can achieve more together.</p>
+              </div>
+              <div class="section-content-item section-content-item-third principle-card card-3">
+                  <h2 class="number">3</h2>
+                  <h3>Always Growing</h3>
+                  <div class="small-divider"></div>
+                  <p>We constantly seek out difficult challenges, share and receive feedback as a gift, and see every failure as an opportunity to grow.</p>
+              </div>
+              <div class="section-content-item section-content-item-third principle-card card-4">
+                  <h2 class="number">4</h2>
+                  <h3>Servant Leadership</h3>
+                  <div class="small-divider"></div>
+                  <p>Great leaders always put others before themselves and engage their community with humility and generosity. We see leadership as a way of improving the world, not simply promoting ourselves.</p>
+              </div>
+              <div class="section-content-item section-content-item-third principle-card card-5">
+                  <h2 class="number">5</h2>
+                  <h3>Solutions First</h3>
+                  <div class="small-divider"></div>
+                  <p>Everything is possible when we are creative and think critically about the problem. We are always thinking of new solutions when faced with difficult challenges.</p>
+              </div>
+              <div class="section-content-item section-content-item-third principle-card card-6">
+                  <h2 class="number">6</h2>
+                  <h3>High Expectations</h3>
+                  <div class="small-divider"></div>
+                  <p>We sweat the small stuff and take pride in what we do.We set goals that others think are impossible and never stop until we achieve them.</p>
+              </div>
+
+          </div>
+        </section>
+
+        <!-- <section class="section">
+          <div class="section-content section-content-plain principles-container">
               <?php $number = 1; ?>
               <?php foreach( get_field('culture_principles') as $principle ): $principle = (object)$principle; ?>
               <div class="section-content-item section-content-item-third principle-card">
@@ -48,7 +91,7 @@ get_header();?>
               </div>
               <?php $number++; endforeach; ?>
           </div>
-        </section>
+        </section> -->
 
         <figure class="full-width-image parallax" style="background-image: url(<?php echo get_field('our_culture_banner_image'); ?>);">
             <div class="section-content full-image-caption animated caption slideInLeft">
