6ce48af4390c07f56d88659d89a0b35eb5f979b0
Schambach Milimu <schambach@circle.co.ke>
2017-03-02 12:01:08 +0300

    just some renaming

--- wp-content/themes/novapioneer/our-culture.php
+++ wp-content/themes/novapioneer/our-culture.php
@@ -1,16 +1,14 @@
 <?php
-/**
-* Template Name: Our Culture
-*
-*/
-
-get_header();?>
+/** 
+ * Template Name: Culture Page
+ */
+get_header(); ?>
 
 <?php if( have_posts() ): ?>
 
+    <?php while( have_posts() ): the_post(); ?>
 
-
-        <section class="section section-hero about-np" data-type="background" data-speed="4">
+        <section class="section section-hero" <?php if(has_post_thumbnail()): echo 'style="background-image: url(' . wp_get_attachment_image_src( get_post_thumbnail_id( ), 'single-post-thumbnail' )[0] . ');"'; endif; ?> data-type="background" data-speed="4">
             <div class="container hero-container">
                 <div class="main-callout-box">
                     <hr>
@@ -20,75 +18,28 @@ get_header();?>
 
         <div class="trigger"></div>
 
-        <section class="section">
+        <section class="section even-section">
             <article class="article article-inner article-inner-alt">
-                <h2 class="centered-title">Our School &amp; Organisation Culture</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+                <h2 class="centered-title">School &amp; Organisation Culture</h2>
+                <?php echo get_field('our_culture'); ?>
             </article>
         </section>
 
-      <section class="section">
-          <div class="section-content section-content-plain principles-container">
-              <div class="section-content-item section-content-item-third principle-card card-1">
-                  <h2 class="number">1</h2>
-                  <h3>Joy of Learning</h3>
-                  <div class="small-divider"></div>
-                  <p>We are life-long learners and we are fueled by curiosity and discovery</p>
-              </div>
-              <div class="section-content-item section-content-item-third principle-card card-2">
-                  <h2 class="number">2</h2>
-                  <h3>Greater Together</h3>
-                  <div class="small-divider"></div>
-                  <p>We constantly support our teammates because we know that we can achieve more together.</p>
-              </div>
-              <div class="section-content-item section-content-item-third principle-card card-3">
-                  <h2 class="number">3</h2>
-                  <h3>Always Growing</h3>
-                  <div class="small-divider"></div>
-                  <p>We constantly seek out difficult challenges, share and receive feedback as a gift, and see every failure as an opportunity to grow.</p>
-              </div>
-              <div class="section-content-item section-content-item-third principle-card card-4">
-                  <h2 class="number">4</h2>
-                  <h3>Servant Leadership</h3>
-                  <div class="small-divider"></div>
-                  <p>Great leaders always put others before themselves and engage their community with humility and generosity. We see leadership as a way of improving the world, not simply promoting ourselves.</p>
-              </div>
-              <div class="section-content-item section-content-item-third principle-card card-5">
-                  <h2 class="number">5</h2>
-                  <h3>Solutions First</h3>
-                  <div class="small-divider"></div>
-                  <p>Everything is possible when we are creative and think critically about the problem. We are always thinking of new solutions when faced with difficult challenges.</p>
-              </div>
-              <div class="section-content-item section-content-item-third principle-card card-6">
-                  <h2 class="number">6</h2>
-                  <h3>High Expectations</h3>
-                  <div class="small-divider"></div>
-                  <p>We sweat the small stuff and take pride in what we do.We set goals that others think are impossible and never stop until we achieve them.</p>
-              </div>
-
-          </div>
+        <section class="section even-section">
+            <div class="section-content section-content-plain principles-container">
+                <?php $number = 1; ?>
+                <?php foreach( get_field('culture_principles') as $principle ): $principle = (object)$principle; ?>
+                <div class="section-content-item section-content-item-third principle-card">
+                    <h2 class="number"><?php echo $number; ?></h2>
+                    <h3><?php echo $principle->title; ?></h3>
+                    <div class="small-divider"></div>
+                    <p class="description"><?php echo $principle->description; ?></p>
+                </div>
+                <?php endforeach; ?>
+            </div>   
         </section>
-
+    <?php endwhile; ?>
 
     <?php get_template_part('includes/partials/content', 'stay-updated'); ?>
-
-
 <?php endif; ?>
-
 <?php get_footer(); ?>
-
-
-<!-- smooth scroll
-<script>
-   $('a[href^="#"]').on('click', function(event) {
-       var target = $(this.getAttribute('href'));
-       if( target.length ) {
-           event.preventDefault();
-           $('html, body').stop().animate({
-               scrollTop: target.offset().top
-           }, 1000);
-       }
-   });
-</script>-->
