2b6b17636034ebb3dac9cebfb603a2f138089129
Schambach Milimu <schambach@circle.co.ke>
2017-03-09 13:00:26 +0300

    a small change

--- wp-content/themes/novapioneer/open-positions.php
+++ wp-content/themes/novapioneer/open-positions.php
@@ -7,7 +7,7 @@ get_header(); ?>
 <section class="section trigger-offset">
     <article>
       <header class="article-header">
-          <h1 class="nav-bar-below">Careers</h1>
+          <h1 class="nav-bar-below"><?php the_title(); ?></h1>
       </header>
         <iframe name="resumator-job-frame" id="resumator-job-frame" class="resumator-advanced-widget" src="//novaacademies.applytojob.com/apply/jobs/" width="100%" scrolling="no" frameborder="0" allowTransparency="true"></iframe><script>function resizeResumatorIframe(height,nojump){if(nojump== 0){window.scrollTo(0,0);}document.getElementById("resumator-job-frame").height = parseInt(height)+20;}</script>
     </article>
