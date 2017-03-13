54fa0f368b4b2f6dbc25c721707d331c53468b2c
edgar chris <edgarchris4@gmail.com>
2017-03-13 13:36:37 +0300

    make iframe scrollable

--- kenya/wp-content/themes/novapioneer/open-positions.php
+++ kenya/wp-content/themes/novapioneer/open-positions.php
@@ -6,10 +6,24 @@ get_header(); ?>
 
 <section class="section trigger-offset">
     <article>
-      <header class="article-header">
-          <h1 class="nav-bar-below"><?php the_title(); ?></h1>
-      </header>
-        <iframe name="resumator-job-frame" id="resumator-job-frame" class="resumator-advanced-widget" src="//novaacademies.applytojob.com/apply/jobs/" height="700px" width="100%" scrolling="no" frameborder="0" allowTransparency="true"></iframe><script>function resizeResumatorIframe(height,nojump){if(nojump== 0){window.scrollTo(0,0);}document.getElementById("resumator-job-frame").height = parseInt(height)+20;}</script>
+        <header class="article-header">
+            <h1 class="nav-bar-below"><?php the_title(); ?></h1>
+        </header>
+        <iframe name="resumator-job-frame" id="resumator-job-frame" class="resumator-advanced-widget"
+                src="//novaacademies.applytojob.com/apply/jobs/"  height="700px" width="100%" frameborder="0"
+                allowTransparency="true" scrolling="yes"></iframe>
+        <script>
+
+
+
+                        function resizeResumatorIframe(height, nojump) {
+                            if (nojump == 0) {
+                                window.scrollTo(0, 0);
+                            }
+                            document.getElementById("resumator-job-frame").height = parseInt(height) + 20;
+                        }
+                    </script>
+
     </article>
 </section>
 
