4b8b574675d30e47e8d89fc52ca5cb4afefb9d52
mark muema <mark@circle.co.ke>
2017-03-13 09:08:32 +0300

    added a title to school notice

--- kenya/wp-content/themes/novapioneer/front-page.php
+++ kenya/wp-content/themes/novapioneer/front-page.php
@@ -191,6 +191,7 @@ get_header(); ?>
                 <div class="large-notice-container">
                     <div class="large-notice">
                         <div class="notice-content">
+                          <h1>Learn About Our Admission Process</h1>
                             <?php echo get_field('admission_call_to_action'); ?>
                             <p class="call-to-action"><a href="<?php echo site_url('/admission-process')?>" class="button button-tiny button-secondary" >Apply Now</a></p>
                         </div>
