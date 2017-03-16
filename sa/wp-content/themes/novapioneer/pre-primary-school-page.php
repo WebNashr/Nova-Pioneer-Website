df2490bc5d4bc8479ef893e50437e36128db6cdb
mark muema <mark@circle.co.ke>
2017-03-16 15:46:24 +0300

    added building block templates for south africa

---
+++ sa/wp-content/themes/novapioneer/pre-primary-school-page.php
@@ -0,0 +1,59 @@
+<?php
+/**
+* Template Name: Pre-primary School Page
+*/
+
+get_header();?>
+
+
+        <section class="section section-hero about-np" data-enllax-ratio="0.1">
+            <div class="container hero-container">
+                <div class="main-callout-box">
+                    <hr>
+                    <h1 class="animated-title">Primary School Building Blocks</h1>
+                </div>
+            </div>
+        </section>
+
+        <div class="trigger"></div>
+
+        <section class="section">
+            <article class="article article-body general-content">
+                <h2 class="centered-title"> The DNA of any Nova Pioneer Primary classroom</h2>
+                <p>Nova Pioneer’s unique approach to education will make you reimagine what is possible for your child. Rooted in decades of learning science, and best models in the world, ours is a student-centric approach that develops leaders and innovators who are equipped to shape the world they envision.</p>
+
+                <h2 class="centered-title">Morning Meetings</h2>
+
+                <p>As students are welcomed into their learning environments we expect them to be excited to experience joyful learning. Each day commences with a morning meeting where learners and teachers are welcomed into the classroom. These meetings set the tone for each day, develop classroom culture and build a sense of community. Here, students share personal thoughts and experiences which provide teachers with contexts for understanding each individual student. Responsive classroom practices ensure respectful and inclusive environments to build the confidence of all students.</p>
+                <br><br>
+
+                <h2 class="centered-title">Collaboration</h2>
+                <p>Collaboration is an essential 21st century skill and developing it starts early. The term global village is never more relevant than in the school of today, and more so in the world that our students will enter. Building relationships in critical conversations and debate, sharing and group probing solving supports ethical action and personal leadership. The collaborative arrangement of the classroom space encourages this conversation and positive argument, and grows adaptive servant leaders through consistent interaction with peers, teachers and the community.</p>
+                <br><br>
+
+                <h2 class="centered-title">Blended Learning</h2>
+                <p>Blended learning on Chromebooks further enhances learning opportunities on a digital platform. Cit apicitio exerumq uiatur, ipid untum labor rerspit aturehenim re officae eumquiam hit eum repeliquae sinis alibus, simet inciur accullab in ent poresti onsequatia dendunt alitatur? Giaspel ipsande re ilignatur, si oditati onseque labo. Nequiae. Nam, sinctatem fuga. Sit hit, offic te et volor raniscimusam, tem.</p>
+                <br><br>
+
+                <h2 class="centered-title">Investigations</h2>
+                <p>In investigations, the natural curiosity and wonder of the student is engaged to create rich learning opportunities, where the teacher is the co-collaborator and learner along with the students and documents the students’ exploration process. Students are invited to ask “why”, to share theories, discuss possibilities, have opportunity to research and investigate outcomes in groups and with teachers. The students’ enquiries are used by the teachers to extract and make connections to the curriculum to enhance the students’ understanding. Investigations are documented in notes, photos and video to be used for further engagement and deeper learning from the student’s interests.</p>
+                <br><br>
+
+                <h2 class="centered-title">Team Teaching</h2>
+                <p>Legere partiendo iracundia at eos, an quo error interesset. Inani dolorem qui et, nonumes voluptua duo ex. Te enim decore quodsi vim, elit bonorum ei mei. Ad nam sint maiestatis scripserit. Aliquam mentitum at eos, eu nec summo iudicabit. Pri ea quot percipit delicatissimi, ne labores ocurreret pro.</p>
+                <br><br>
+
+                <h2 class="centered-title">Vigorous literacy / numeracy</h2>
+                <p>Legere partiendo iracundia at eos, an quo error interesset. Inani dolorem qui et, nonumes voluptua duo ex. Te enim decore quodsi vim, elit bonorum ei mei. Ad nam sint maiestatis scripserit. Aliquam mentitum at eos, eu nec summo iudicabit. Pri ea quot percipit delicatissimi, ne labores ocurreret pro.</p>
+                <br><br>
+
+            </article>
+      </section>
+
+
+
+
+    <?php get_template_part('includes/partials/content', 'stay-updated'); ?>
+
+
+<?php get_footer(); ?>
