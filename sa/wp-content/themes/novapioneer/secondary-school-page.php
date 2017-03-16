1a2b3a0a95fe09c8ab463e1b338964f7200b3201
mark muema <mark@circle.co.ke>
2017-03-16 17:22:34 +0300

    added images to the NP building block templates

--- sa/wp-content/themes/novapioneer/secondary-school-page.php
+++ sa/wp-content/themes/novapioneer/secondary-school-page.php
@@ -18,28 +18,33 @@ get_header();?>
         <div class="trigger"></div>
 
         <section class="section">
-            <article class="article article-body general-content">
+            <article class="article article-inner article-inner-alt">
+              <img src="<?php echo novap_get_baseurl(); ?>/img/image-wide-1-sa.jpg" alt="">
                 <h2 class="centered-title"> The DNA of any Nova Pioneer Primary classroom</h2>
                 <p>Nova Pioneer’s unique approach to education will make you reimagine what is possible for your child. Rooted in decades of learning science, and best models in the world, ours is a student-centric approach that develops leaders and innovators who are equipped to shape the world they envision.</p>
 
                 <h2 class="centered-title">Multi-Disciplinary Projects</h2>
-
+                <img src="<?php echo novap_get_baseurl(); ?>/img/image-wide-1-sa.jpg" alt="">
                 <p>Each term, students complete group projects in which they apply their knowledge and problem-solving skills from multiple courses towards investigating, researching, and presenting information and findings on a particular topic. These projects include topics such as urban planning &amp; transportation, ancient societies and innovations throughout history, biodiversity and human impacts, and water quality and conservation. By exploring these topics, students are able to combine ideas and approaches from different subjects, allowing them to engage in the multidisciplinary thinking that will prepare them for environments where the crosspollinization of ideas leads to success.</p>
                 <br><br>
 
                 <h2 class="centered-title">Technology in Education</h2>
+                <img src="<?php echo novap_get_baseurl(); ?>/img/image-wide-1-sa.jpg" alt="">
                 <p>At Nova Pioneer, we incorporate the use of technology in meaningful way throughout the learners experience. Teachers uitlise videos, images, and realworld data demonstrations to apply and review course material. To make the material come to life, students play interactive games and participate in virtual labs to apply and review course material Nova Pioneer students learn real-world use of software and hardware through creating professional presentations,writing reports, and designing code.</p>
                 <br><br>
 
                 <h2 class="centered-title">Global Exposure</h2>
+                <img src="<?php echo novap_get_baseurl(); ?>/img/image-wide-1-sa.jpg" alt="">
                 <p>The world that our students will enter after graduation is one in which they will be working and engaging with counterparts from around the world. At Nova Pioneer, we prepare our students for this future by ensuring that they have exposure to working with other students from across the continent, and around the world. Nova Pioneer students engage in international projects with peers in other countries to share data and ideas. Students also get the opportunity to meet with leaders in business, government, the arts, and technology to learn more about the opportunities that await them after school.</p>
                 <br><br>
 
                 <h2 class="centered-title">Exploration &amp; Inquiry</h2>
+                <img src="<?php echo novap_get_baseurl(); ?>/img/image-wide-1-sa.jpg" alt="">
                 <p>Exploration is the core of every single classroom at Nova Pioneer. Teachers spend more time asking “why?” than telling students “what”. Students first explore information on their own with guided questions, allowing them to draw their own connections with the course material. This solidifies their understanding of the course information, because they’ve not only heard or read it, but they’ve discovered the answer on their own.</p>
                 <br><br>
 
                 <h2 class="centered-title">Innovation</h2>
+                <img src="<?php echo novap_get_baseurl(); ?>/img/image-wide-1-sa.jpg" alt="">
                 <p>Our students have dedicated time each week in which they learn new methods for solving real-world problems by using creative approaches and use of technology. These sessions are largely student-led and initiated, guided by an instructor, with students choosing their own topics to explore and proposing their own unique solutions. Students work from ideation, through prototyping, eventually building their solutions and in some cases deploying them on the campus and beyond.</p>
                 <br><br>
 
