4af0df9a9c9ddce027ca8b52190c43d1db7ae0bb
mark muema <mark@circle.co.ke>
2017-03-17 11:31:09 +0300

    small adjustment to school notice and school list item

--- sa/wp-content/themes/novapioneer/school-home-page.php
+++ sa/wp-content/themes/novapioneer/school-home-page.php
@@ -66,8 +66,7 @@ get_header(); ?>
                         <div class="notice-content">
                             <h1>Learn About Our Admission Process</h1>
                             <?php echo get_field('admission_call_to_action'); ?>
-                            <p class="call-to-action"><a href="<?php echo site_url('/admission-process')?>" class="button button-small button-secondary">Apply
-                                    Now</a></p>
+                            <p class="call-to-action"><a href="<?php echo site_url('/admission-process')?>" class="button button-small button-secondary">Admission Process</a></p>
                         </div>
                     </div>
                 </div>
