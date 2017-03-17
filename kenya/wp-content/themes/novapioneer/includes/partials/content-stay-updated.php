3a487ad0f180c35892ed9db404542f182d1b9cdd
mark muema <mark@circle.co.ke>
2017-03-17 05:39:50 +0300

    adjustments to stay updated form

--- kenya/wp-content/themes/novapioneer/includes/partials/content-stay-updated.php
+++ kenya/wp-content/themes/novapioneer/includes/partials/content-stay-updated.php
@@ -9,7 +9,7 @@
                 <p>Enter your email address below and receive the latest Nova Pioneer news, upcoming events and admission opportunities.</p>
             </header>
             <div class="subscribe-form-container">
-            <?php echo do_shortcode('[gravityform id="5" title="false" description="false" tabindex="50"]'); ?>
+            <?php echo do_shortcode('[gravityform id="6" title="false" description="false" tabindex="50"]'); ?>
           </div>
         </div>
     </div>
