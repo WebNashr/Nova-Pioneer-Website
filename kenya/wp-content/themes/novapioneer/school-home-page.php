5cfba7224294ac2a70e89a5f653390fbee7ec69e
mark muema <mark@circle.co.ke>
2017-03-23 08:56:47 +0300

    updated slider settings on school homepages

--- kenya/wp-content/themes/novapioneer/school-home-page.php
+++ kenya/wp-content/themes/novapioneer/school-home-page.php
@@ -375,37 +375,59 @@ get_header(); ?>
 </script> -->
 <script type="text/javascript">
     $(document).ready(function () {
-        $("#testimonial-slider").lightSlider({
-            item: 1,
-            autoWidth: true,
-            slideMove: 1, // slidemove will be 1 if loop is true
-            slideMargin: 500,
+      $("#testimonial-slider").lightSlider({
+          item: 1,
+          autoWidth: false,
+          slideMove: 1, // slidemove will be 1 if loop is true
+          // slideMargin: 300, //500
 
-            addClass: '',
-            mode: "slide",
-            useCSS: true,
-            cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-            easing: 'linear', //'for jquery animation',////
+          addClass: '',
+          mode: "slide",
+          useCSS: true,
+          cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+          easing: 'linear', //'for jquery animation',////
 
-            speed: 400, //ms'
-            auto: false,
-            loop: true,
-            slideEndAnimation: true,
-            pause: 2000,
+          speed: 400, //ms'
+          auto: false,
+          loop: true,
+          // slideEndAnimation: true,
+          pause: 2000,
 
-            keyPress: false,
-            controls: true,
-            prevHtml: '',
-            nextHtml: '',
+          keyPress: false,
+          controls: true,
+          prevHtml: '',
+          nextHtml: '',
+          addClass:'content-slider',
 
-            currentPagerPosition: 'middle',
+          // currentPagerPosition: 'middle',
 
-            enableTouch: true,
-            enableDrag: true,
-            freeMove: true,
-            swipeThreshold: 40,
+          enableTouch: true,
+          enableDrag: true,
+          freeMove: true,
+          // swipeThreshold: 40,
+          responsive: [
+            // {
+            //     breakpoint: 1024,
+            //     settings: {
+            //         slideMargin: 500,
+            //     }
+            // },
+            //   {
+            //       breakpoint: 800,
+            //       settings: {
+            //           slideMargin: 500,
+            //       }
+            //   },
 
-        });
+              {
+                  breakpoint: 320,
+                  settings: {
+                      slideMargin: 245,
+                  }
+              }
+          ]
+
+      });
     });
 </script>
 <?php get_footer(); ?>
