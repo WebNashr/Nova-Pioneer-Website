fb72036ae816dfc25fb8be802cb8ed9f69584b01
edgar chris <edgarchris4@gmail.com>
2017-03-29 15:48:09 +0300

    remove separators

--- kenya/wp-content/themes/novapioneer/meet-the-students.php
+++ kenya/wp-content/themes/novapioneer/meet-the-students.php
@@ -16,70 +16,78 @@ get_header(); ?>
 
     <?php while (have_posts()): the_post(); ?>
 
-            <section class="section section-hero corporate-hero" <?php if (has_post_thumbnail()): echo 'style="background-image: url(' . wp_get_attachment_image_src(get_post_thumbnail_id(), 'single-post-thumbnail')[0] . ');"'; endif; ?> data-enllax-ratio="0.1">
-                <div class="container hero-container">
-                    <div class="main-callout-box">
-                        <hr>
-                        <h1 class="animated-title"><?php  the_title()?></h1>
-                        <p><?php the_content()?></p>
-                    </div>
+        <section
+            class="section section-hero corporate-hero" <?php if (has_post_thumbnail()): echo 'style="background-image: url(' . wp_get_attachment_image_src(get_post_thumbnail_id(), 'single-post-thumbnail')[0] . ');"'; endif; ?>
+            data-enllax-ratio="0.1">
+            <div class="container hero-container">
+                <div class="main-callout-box">
+                    <hr>
+                    <h1 class="animated-title"><?php the_title() ?></h1>
+                    <p><?php the_content() ?></p>
                 </div>
-            </section>
-            <div class="trigger"></div>
+            </div>
+        </section>
+        <div class="trigger"></div>
 
-            <section class="section">
-                <div class="page-navigation-container meet-students-nav">
-                    <div class="navigation-wrap">
-                        <div class="section-title">
-                            <h3>Our Students</h3>
-                        </div>
-                        <div class="links-inner-wrap"
-                        <div class="section-content-item">
-                            <div class="anchor-link">
-                                <a href="#primary" class="" title="">Primary Students</a>
-                            </div>
-                            <div class="link-separator">&nbsp;</div>
-                            <div class="anchor-link">
-                                <a href="#secondary" class="" title="">Secondary Students</a>
-                            </div>
-                            <div class="link-separator">&nbsp;</div>
-                            <div class="anchor-link">
-                                <a href="#blog" class="" title="">Student Blog</a>
-                            </div>
-                            <div class="link-separator">&nbsp;</div>
-                             <div class="anchor-link">
-                                 <a href="#contact-student" class="" title="">Contact a Student</a>
-                             </div>
+        <section class="section">
+            <div class="page-navigation-container meet-students-nav">
+                <div class="navigation-wrap">
+                    <div class="section-title">
+                        <h3>Our Students</h3>
+                    </div>
+                    <div class="links-inner-wrap">
+                    <div class="section-content-item">
+                        <div class="anchor-link">
+                            <a href="#primary" class="" title="">Primary Students</a>
                         </div>
                     </div>
+                    <div class="section-content-item">
+                        <div class="anchor-link">
+                            <a href="#secondary" class="" title="">Secondary Students</a>
+                        </div>
+                    </div>
+                    <div class="section-content-item">
+                        <div class="anchor-link">
+                            <a href="#blog" class="" title="">Student Blog</a>
+                        </div>
+                    </div>
+                    <div class="section-content-item">
+                        <div class="anchor-link">
+                            <a href="#contact-student" class="" title="">Contact a Student</a>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </section>
+            </div>
+            </div>
+            </div>
+        </section>
 
 
-            <span class="anchor-link" id="primary"></span>
+        <span class="anchor-link" id="primary"></span>
 
 
-            <section class="section section-pair">
-                <div class="section-navigation">
-                    <h2 id="primary">Primary Students</h2>
-                </div>
+        <section class="section section-pair">
+            <div class="section-navigation">
+                <h2 id="primary">Primary Students</h2>
+            </div>
 
-                <div class="section-content">
-                    <?php
-                    if( have_rows('juniors') ):  while ( have_rows('juniors') ) : the_row();?>
+            <div class="section-content">
+                <?php
+                if (have_rows('juniors')): while (have_rows('juniors')) : the_row(); ?>
                     <div class="the-students">
 
                         <div class="section-content-item section-content-item-half">
-                            <h3><?php  the_sub_field('student_name');?></h3>
-                            <h4 class="grade"><?php  the_sub_field('student_grade');?></h4>
+                            <h3><?php the_sub_field('student_name'); ?></h3>
+                            <h4 class="grade"><?php the_sub_field('student_grade'); ?></h4>
                             <div class="testimonial pull-quote">
                                 <blockquote>
                                     <svg aria-hidden="true">
-                                        <use xlink:href="<?php echo novap_get_baseurl(); ?>/img/quote-mark-icon.svg#quote-mark"></use>
+                                        <use
+                                            xlink:href="<?php echo novap_get_baseurl(); ?>/img/quote-mark-icon.svg#quote-mark"></use>
                                     </svg>
-                                    <?php  the_sub_field('remarks');?>
-                                        <cite><span><?php  the_sub_field('student_name');?>,</span> Nova Pioneer Student</cite>
+                                    <?php the_sub_field('remarks'); ?>
+                                    <cite><span><?php the_sub_field('student_name'); ?>,</span> Nova Pioneer
+                                        Student</cite>
                                 </blockquote>
                             </div>
                         </div>
@@ -97,108 +105,114 @@ get_header(); ?>
                             </div>
                         </div>
                     </div>
-                    <?php   endwhile;  endif;?>
-                </div>
-            </section>
+                <?php endwhile; endif; ?>
+            </div>
+        </section>
 
 
-
-            <section class="full-width-image-container" data-enllax-type="foreground">
-            <figure class="full-width-image secondary-bgd-image parallax" <?php if (get_field('parallax_image')): echo 'style="background-image: url(' . get_field('parallax_image') . ');"'; endif; ?>  data-enllax-ratio="0.2">
+        <section class="full-width-image-container" data-enllax-type="foreground">
+            <figure
+                class="full-width-image secondary-bgd-image parallax" <?php if (get_field('parallax_image')): echo 'style="background-image: url(' . get_field('parallax_image') . ');"'; endif; ?>
+                data-enllax-ratio="0.2">
                 <div class="section-content full-image-caption animated caption">
                     <figcaption>
-                        <p><?php the_field('parallax_text')?></p>
+                        <p><?php the_field('parallax_text') ?></p>
                     </figcaption>
                 </div>
                 <span class="anchor-link" id="preprimary"></span>
             </figure>
-            </section>
+        </section>
 
 
-            <span class="anchor-link"></span>
+        <span class="anchor-link"></span>
 
 
-            <section class="section section-pair">
-                <div class="section-navigation">
-                    <h2 id="secondary">Secondary Students</h2>
-                </div>
+        <section class="section section-pair">
+            <div class="section-navigation">
+                <h2 id="secondary">Secondary Students</h2>
+            </div>
 
-                <div class="section-content">
-                    <?php
-                    if( have_rows('seniors') ):  while ( have_rows('seniors') ) : the_row();?>
-                        <div class="the-students">
+            <div class="section-content">
+                <?php
+                if (have_rows('seniors')): while (have_rows('seniors')) : the_row(); ?>
+                    <div class="the-students">
 
-                            <div class="section-content-item section-content-item-half">
-                                <h3><?php  the_sub_field('student_name');?></h3>
-                                <h4 class="grade"><?php  the_sub_field('student_grade');?></h4>
-                                <div class="testimonial pull-quote">
-                                    <blockquote>
-                                        <svg aria-hidden="true">
-                                            <use xlink:href="<?php echo novap_get_baseurl(); ?>/img/quote-mark-icon.svg#quote-mark"></use>
-                                        </svg>
-                                        <?php  the_sub_field('remarks');?>
-                                            <cite><span><?php  the_sub_field('student_name');?>,</span> Nova Pioneer Student</cite>
-                                    </blockquote>
-                                </div>
-                            </div>
-
-
-                            <div class="section-content-item section-content-item-half">
-                                <div class="media youtube-video">
-                                    <?php if (get_sub_field('video_or_image') == 'image') : ?>
-
-                                        <img src="<?php the_sub_field('image') ?>"/>
-                                    <?php else : ?>
-                                        <iframe width="347" height="194" src="<?php the_sub_field('video_link'); ?>"
-                                                frameborder="0" allowfullscreen></iframe>
-                                    <?php endif ?>
-                                </div>
+                        <div class="section-content-item section-content-item-half">
+                            <h3><?php the_sub_field('student_name'); ?></h3>
+                            <h4 class="grade"><?php the_sub_field('student_grade'); ?></h4>
+                            <div class="testimonial pull-quote">
+                                <blockquote>
+                                    <svg aria-hidden="true">
+                                        <use
+                                            xlink:href="<?php echo novap_get_baseurl(); ?>/img/quote-mark-icon.svg#quote-mark"></use>
+                                    </svg>
+                                    <?php the_sub_field('remarks'); ?>
+                                    <cite><span><?php the_sub_field('student_name'); ?>,</span> Nova Pioneer
+                                        Student</cite>
+                                </blockquote>
                             </div>
                         </div>
-                    <?php   endwhile;  endif;?>
 
-                </div>
-            </section>
-            <section class="section section-pair section-school-blog even-section">
-                <div class="section-navigation">
-                    <h2 id="blog">Student Blog</h2>
-                </div>
 
-                    <div class="section-content">
-                        <?php foreach (get_field('student_blogs') as $blog_post): $blog_post = (object)$blog_post; ?>
+                        <div class="section-content-item section-content-item-half">
+                            <div class="media youtube-video">
+                                <?php if (get_sub_field('video_or_image') == 'image') : ?>
 
-                            <div class="section-content-item section-content-item-third blog-article">
-                                <a href="<?php echo $blog_post->url; ?>" target="_blank"><div class="blog-article-image" style="background-image: url(<?php echo $blog_post->picture; ?>);"
-                                        title="<?php echo $blog_post->title; ?>"></div></a>
-                                <div class="blog-summary-container">
-                                <a href="<?php echo $blog_post->url; ?>" target="_blank" class="blog-article-title"
-                                    title="<?php echo $blog_post->title; ?>"><?php echo $blog_post->title; ?></a>
-                                <p class="article-author"><?php echo $blog_post->author; ?></p>
-                                </div>
+                                    <img src="<?php the_sub_field('image') ?>"/>
+                                <?php else : ?>
+                                    <iframe width="347" height="194" src="<?php the_sub_field('video_link'); ?>"
+                                            frameborder="0" allowfullscreen></iframe>
+                                <?php endif ?>
                             </div>
+                        </div>
+                    </div>
+                <?php endwhile; endif; ?>
 
-                        <?php endforeach; ?>
+            </div>
+        </section>
+        <section class="section section-pair section-school-blog even-section">
+            <div class="section-navigation">
+                <h2 id="blog">Student Blog</h2>
+            </div>
+
+            <div class="section-content">
+                <?php foreach (get_field('student_blogs') as $blog_post): $blog_post = (object)$blog_post; ?>
+
+                    <div class="section-content-item section-content-item-third blog-article">
+                        <a href="<?php echo $blog_post->url; ?>" target="_blank">
+                            <div class="blog-article-image"
+                                 style="background-image: url(<?php echo $blog_post->picture; ?>);"
+                                 title="<?php echo $blog_post->title; ?>"></div>
+                        </a>
+                        <div class="blog-summary-container">
+                            <a href="<?php echo $blog_post->url; ?>" target="_blank" class="blog-article-title"
+                               title="<?php echo $blog_post->title; ?>"><?php echo $blog_post->title; ?></a>
+                            <p class="article-author"><?php echo $blog_post->author; ?></p>
+                        </div>
                     </div>
 
-            </section>
+                <?php endforeach; ?>
+            </div>
 
-            
-            <section class="section section-pair contact-student ">
-                <div class="section-navigation">
-                    <h2>Contact a Student</h2>
+        </section>
+
+
+        <section class="section section-pair contact-student ">
+            <div class="section-navigation">
+                <h2>Contact a Student</h2>
+            </div>
+
+            <div class="section-content">
+                <span class="anchor-link" id="contact-student"></span>
+                <div class="section-content-item section-content-item-half">
+                    <p><?php the_field('contact_text') ?></p>
                 </div>
 
-                <div class="section-content">
-                  <span class="anchor-link" id="contact-student"></span>
-                    <div class="section-content-item section-content-item-half">
-                        <p><?php the_field('contact_text')?></p>
-                    </div>
-
-                    <div class="section-content-item section-content-item-full">
-                        <?php echo do_shortcode('[gravityform id="3" title="false" description="false"]'); ?>
-                    </div>
+                <div class="section-content-item section-content-item-full">
+                    <?php echo do_shortcode('[gravityform id="3" title="false" description="false"]'); ?>
                 </div>
-            </section>
+            </div>
+        </section>
 
     <?php endwhile; ?>
 
