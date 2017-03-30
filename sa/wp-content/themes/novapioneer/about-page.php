e4a5e8b2ab4c143bc3f3ef3c3d847193246c23ff
edgar chris <edgarchris4@gmail.com>
2017-03-30 15:45:58 +0300

    add read more/less

--- sa/wp-content/themes/novapioneer/about-page.php
+++ sa/wp-content/themes/novapioneer/about-page.php
@@ -36,35 +36,39 @@ get_header(); ?>
             <article class="article article-inner article-inner-alt mission-vision">
                 <h2 class="centered-title">Our Story</h2>
 
-                <!--<input type="checkbox" class="read-more-state" id="post-1"/>-->
+                <!-- <input type="checkbox" class="read-more-state" id="post-121"/>-->
                 <div class="read-more-wrap">
-                    <p><?php echo array_shift($our_story_paragraphs); ?></p>
+                    <p><?php $counter_s = 0;
+                        echo array_shift($our_story_paragraphs); ?></p>
                     <span class="read-more-target-js" data-collapse="story">
-                           <?php foreach ($our_story_paragraphs as $s_paragraph): ?>
+                           <?php foreach ($our_story_paragraphs as $s_paragraph): $counter_s++ ?>
                                <p><?php echo $s_paragraph; ?></p>
                            <?php endforeach; ?>
                 </span></div>
-                <button class="read-more-trigger-js button button-tiny button-primary" data-collapse="story">
-                    Read More
-                </button>
-
+                <?php if ($counter_s > 0): ?>
+                    <button class="read-more-trigger-js button button-tiny button-primary" data-collapse="story">
+                        Read More
+                    </button>
+                <?php endif; ?>
                 <h2 class="centered-title">Our Vision</h2>
                 <?php echo $our_vision; ?>
 
                 <h2 class="centered-title" id="mission-scroll">Our Mission</h2>
                 <!-- <input type="checkbox" class="read-more-state" id="post-<?php echo get_the_ID(); ?>"/>-->
-
                 <div class="read-more-wrap">
-                    <p><?php echo array_shift($our_mission_paragraphs); ?></p>
+                    <p><?php $counter_m = 0;
+                        echo array_shift($our_mission_paragraphs); ?></p>
                   <span class="read-more-target-js" data-collapse="mission">
-                      <?php foreach ($our_mission_paragraphs as $paragraph): ?>
+                      <?php foreach ($our_mission_paragraphs as $paragraph): $counter_m++; ?>
                           <p><?php echo $paragraph; ?></p>
                       <?php endforeach; ?>
                   </span>
                 </div>
-                <button class="read-more-trigger-js button button-tiny button-primary" data-collapse="mission">
-                    Read More
-                </button>
+                <?php if ($counter_m > 0): ?>
+                    <button class="read-more-trigger-js button button-tiny button-primary" data-collapse="mission">
+                        Read More
+                    </button>
+                <?php endif ?>
 
 
             </article>
@@ -77,7 +81,7 @@ get_header(); ?>
                     data-enllax-ratio="0.2">
                 <div class="section-content full-image-caption animated caption slideInLeft">
                     <figcaption>
-                         <?php echo get_field('our_culture_banner_image_caption'); ?>
+                        <?php echo get_field('our_culture_banner_image_caption'); ?>
                     </figcaption>
                 </div>
             </figure>
@@ -128,9 +132,11 @@ get_header(); ?>
             var offset = 75; //Offset of 75px
             if (readmoreCounter % 2 === 0) {
                 //  do some stuff if you want
+                $(this).text('Read Less');
 
             }
             else {
+                $(this).text('Read More');
                 if (collapse == 'mission') {
                     jQuery('html, body').animate({
                         scrollTop: $('#mission-scroll').offset().top - offset
@@ -149,5 +155,6 @@ get_header(); ?>
         });
     })
 </script>
+
 
 <?php get_footer(); ?>
