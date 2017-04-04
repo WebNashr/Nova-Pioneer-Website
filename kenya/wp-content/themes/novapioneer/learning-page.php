455916337bcbaf3a759b31f6ccdabd1e4b6e01ad
Kumbi Madzinga <kumbi@circle.co.ke>
2017-04-04 14:27:20 +0300

    paaaaaarallllalalalalallalaaaxxxxxx ...alternative for tiny tiny itsy itsy screens prototype 2

--- kenya/wp-content/themes/novapioneer/learning-page.php
+++ kenya/wp-content/themes/novapioneer/learning-page.php
@@ -66,19 +66,15 @@ get_header();?>
         <?php foreach($education_stages as $stage): $stage = (object)$stage; ?>
             <!-- <span class="anchor-link" id="<?php echo strtolower($stage->title); ?>"></span> -->
             <section class="full-width-image-container small-screens">
-                <figure class="full-width-image">
-                    <!--<?php if(get_field('our_culture_banner_image')): ?>-->
-                        <img src="<?php echo $stage->banner_image; ?>" />
-                    <!--<?php endif; ?>-->
-
+                <figure class="full-width-image" style="background-image: url(<?php echo $stage->banner_image; ?>);">
                     <div class="section-content full-image-caption animated caption">
                         <figcaption>
-                            ### show on small ###<br><?php echo $stage->banner_image_caption; ?>
+                            <?php echo $stage->banner_image_caption; ?>
                         </figcaption>
                     </div>
                 </figure>
             </section>
-            
+
             <section class="full-width-image-container large-screens" data-enllax-type="foreground">
                 <figure class="full-width-image <?php echo isOnMobile()->parallax ?>" style="background-image: url(<?php echo $stage->banner_image; ?>);" data-enllax-ratio="<?php echo isOnMobile()->ratio ?>" >
                     <div class="section-content full-image-caption animated caption">
