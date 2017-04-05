576cb9f0ff8b9dfb32bf10409dac4886516c148d
edgar chris <edgarchris4@gmail.com>
2017-04-05 10:49:34 +0300

    fix tricle effect for school head

--- kenya/wp-content/themes/novapioneer/our-team.php
+++ kenya/wp-content/themes/novapioneer/our-team.php
@@ -30,7 +30,7 @@ get_header();?>
                 <figure class="profile">
                     <img src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $school_head->ID ), 'single-post-thumbnail' )[0]; ?>" alt="<?php echo $school_head->post_title; ?>" />
                     <h3 class="profile-name"><?php echo $school_head->post_title; ?></h3>
-                    <h6 class="profile-role">Head of School</h6>
+                    <h6 class="profile-role"><?php the_field('title', $school_head->ID)?></h6>
                 </figure>
             </div>
 
