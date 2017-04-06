df9f8250ea59182b264169dcc2355db44f0ffdb8
Kumbi Madzinga <kumbi@circle.co.ke>
2017-04-06 18:45:17 +0300

    ios menu fix

--- sa/wp-content/themes/novapioneer/header.php
+++ sa/wp-content/themes/novapioneer/header.php
@@ -3,9 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!-- <meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=yes"> -->
-        <!-- <meta name="viewport" content="; initial-scale=1.0; maximum-scale=1.0;"> -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=yes">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, , shrink-to-fit=no">
         <meta name="description" content="<?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?>">
         <meta http-equiv="cleartype" content="on">
         <?php wp_head(); ?>
