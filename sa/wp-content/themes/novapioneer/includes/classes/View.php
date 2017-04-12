4b7d2f8397754bfc392afa8973a87ec84b9c00ca
edgar chris <edgarchris4@gmail.com>
2017-04-12 13:35:26 +0300

    scala valuation fix

--- sa/wp-content/themes/novapioneer/includes/classes/View.php
+++ sa/wp-content/themes/novapioneer/includes/classes/View.php
@@ -17,52 +17,55 @@ use Twig_Environment;
 class View
 {
     /**
-	 * The template loader.
-	 *
-	 * @access   private
-	 * @var      Twig_Loader_Filesystem    $twig_loader
-	 */
+     * The template loader.
+     *
+     * @access   private
+     * @var      Twig_Loader_Filesystem $twig_loader
+     */
     private $twig_loader;
 
     /**
-	 * The twig instance.
-	 *
-	 * @access   private
-	 * @var      Twig_Environment    $twig_loader
-	 */
+     * The twig instance.
+     *
+     * @access   private
+     * @var      Twig_Environment $twig_loader
+     */
     private $twig;
 
     /**
-	 * The twig template.
-	 *
-	 * @access   private
-	 * @var      Twig_Template    $template
-	 */
+     * The twig template.
+     *
+     * @access   private
+     * @var      Twig_Template $template
+     */
     private $template;
 
-    /**
-	 * The path to look for templates.
-	 *
-	 * @const      TEMPLATE_PATH
-	 */
-    const TEMPLATE_PATH = NOVAP_THEME_PATH . 'templates';
+    const TEMPLATE_DIR = 'templates';
+    const CACHE_DIR = 'cache/twig';
 
     /**
-	 * The path to cache compiled templates.
-	 *
-	 * @const      CACHE_PATH
-	 */
-    const CACHE_PATH = NOVAP_THEME_PATH . 'cache/twig';
+     * The path to look for templates.
+     *
+     * @const      TEMPLATE_PATH
+     */
+    const TEMPLATE_PATH = NOVAP_THEME_PATH . self::TEMPLATE_DIR;
 
-	/**
-	 * Initialize twig and load template.
-	 */
+    /**
+     * The path to cache compiled templates.
+     *
+     * @const      CACHE_PATH
+     */
+    const CACHE_PATH = NOVAP_THEME_PATH . self::CACHE_DIR;
+
+    /**
+     * Initialize twig and load template.
+     */
     public function __construct($template_name)
     {
 
         Twig_Autoloader::register();
 
-        $this->twig_loader = new Twig_Loader_Filesystem( self::TEMPLATE_PATH );
+        $this->twig_loader = new Twig_Loader_Filesystem(self::TEMPLATE_PATH);
 
         $this->twig = new Twig_Environment($this->twig_loader, array(
             'cache' => self::CACHE_PATH,
@@ -72,29 +75,26 @@ class View
         $this->load_template($template_name); // TODO: check if template file actually exists and throw an error if it doesn't
     }
 
-	/**
-	 * Renders the template.
-	 */
+    /**
+     * Renders the template.
+     */
     public function render(array $vars, $should_echo = true)
     {
-		if($should_echo)
-		{
-			echo $this->template->render($vars);
-		}
-		else
-		{
-			return $this->template->render($vars);
-		}
-        
+        if ($should_echo) {
+            echo $this->template->render($vars);
+        } else {
+            return $this->template->render($vars);
+        }
+
     }
 
-	/**
-	 * Loads the template.
-	 */
+    /**
+     * Loads the template.
+     */
     private function load_template($template_name)
     {
         $this->template = $this->twig->loadTemplate($template_name);
     }
 
-    
+
 }
