692e2559c948a5139bf74f8379d0b0dc0d228f7e
edgar chris <edgarchris4@gmail.com>
2017-06-08 15:58:23 +0300

    add GTM tags

--- index.php
+++ index.php
@@ -25,6 +25,15 @@ require_once __DIR__ . '/redirect.php';
     <!-- site css -->
     <link rel="stylesheet" media="all" type="text/css" href="/assets/css/main.min.css"/>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W89TX54');</script>
+    <!-- End Google Tag Manager -->
+
+
     <!-- Google Analytics -->
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function()
@@ -50,6 +59,10 @@ require_once __DIR__ . '/redirect.php';
 </head>
 
 <body class="front-page body-home">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W89TX54"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!-- page-header -->
 <header class="page-header page-header-home" id="header-container">
--- kenya/wp-content/themes/novapioneer/header.php
+++ kenya/wp-content/themes/novapioneer/header.php
@@ -9,6 +9,22 @@
     <meta http-equiv="cleartype" content="on">
     <?php wp_head(); ?>
 
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-W89TX54');</script>
+    <!-- End Google Tag Manager -->
+
+
     <?php if (novap_ga_tracking_id()): ?>
         <!-- Google Analytics -->
         <script>
@@ -65,6 +81,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W89TX54"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <!-- page-modal -->
 <div class="modal">
     <div class="modal-logo">
