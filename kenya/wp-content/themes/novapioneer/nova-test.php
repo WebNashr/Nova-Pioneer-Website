10438bcd8683c33eb46c9969d1e86b3a2d4770c6
edgar chris <edgarchris4@gmail.com>
2017-08-23 14:48:21 +0300

    modified new template

--- kenya/wp-content/themes/novapioneer/nova-test.php
+++ kenya/wp-content/themes/novapioneer/nova-test.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <?php while (have_posts()): the_post(); ?>
         <h1>    <?php the_title(); ?></h1>
 
-        <?php the_content(); ?>
+        <p><?php the_content(); ?></p>
 
     <?php endwhile; ?>
 
