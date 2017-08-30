dda531aa492e9215956e9a007d6a5024271405a2
dwachiranovapioneer <dwachira@novapioneer.com>
2017-08-30 10:22:01 +0300

    Update kili-for-the-kids-page.php

--- kenya/wp-content/themes/novapioneer/kili-for-the-kids-page.php
+++ kenya/wp-content/themes/novapioneer/kili-for-the-kids-page.php
@@ -90,7 +90,7 @@ get_header();?>
                     <?php endforeach; ?> -->
 
                     <div class="button-wrap">
-                        <a href="http://novaacademies.applytojob.com/" target="_blank" class="button button-default button-primary" title="">View Open Positions</a>
+                        <a href="https://www.generosity.com/education-fundraising/kilimanjaro-4-the-kids-innovative-stem-programmes" target="_blank" class="button button-default button-primary" title="">Donate to K4TK</a>
                     </div>
                 </div>
             </article>
