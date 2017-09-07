aa1927a63428ae7dd804a15c450faa5696a3b2df
edgar chris <edgarchris4@gmail.com>
2017-09-07 15:29:55 +0300

    headerlink

--- kenya/wp-content/themes/novapioneer/nova-test.php
+++ kenya/wp-content/themes/novapioneer/nova-test.php
@@ -8,7 +8,8 @@ get_header(); ?>
 <?php if (have_posts()): ?>
 
     <?php while (have_posts()): the_post(); ?>
-        <h1>    <?php the_title(); ?></h1>
+        <h1><?php the_title(); ?></h1>
+        <h2> header link</h2>
 
         <p><?php the_content(); ?></p>
 
