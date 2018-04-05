f61542a5703845ceb39e4787acf4d7ff58edc508
Young Mwanzia <young@circle.co.ke>
2018-04-05 18:51:24 +0300

    Dynamic support functions template

--- kenya/wp-content/themes/novapioneer/Careers-Support-Functions(2018)-static.3.php
+++ kenya/wp-content/themes/novapioneer/Careers-Support-Functions(2018)-static.3.php
@@ -28,84 +28,84 @@ get_header();?>
             </div>
         </section>
 
+
+        <section class="section">
+            <?php //while(); ?>
+                <article class="article">
+                <p><?php echo the_content(); ?></p>
+                </article>
+            <?php //endwhile; wp_reset_postdata(); ?>
+        </section>
+
+        <?php 
+            $info = 'info';
+            if(have_rows($info)):
+        ?>
+
          <section class="section">
                 <h1>Who may apply?</h1>
                 <p>You may apply if you:</p> <br>
             <article class="article ">
             <div class="card-container steps-container">
+                <?php while(have_rows($info)): the_row(); ?>
                 <div class="card admission-step">
-                <h1>-1-</h1>
-                <p>are pursuing a teaching degree/qualification</p>
-            </div>
-
-            <div class="card admission-step">
-                <h1>-2-</h1>
-                <p>are excited to grow, develop, reflect and constantly refine your teaching
-                    practice</p>
-            </div>
-
-            <div class="card admission-step">
-                <h1>-3-</h1>
-                <p>have a high level of detail-orientation, organization and professionalism</p>
-            </div>
-
-            <div class="card admission-step">
-                <h1>-4-</h1>
-                <p>are eager to think outside the box and contribute to the growth of a startup
-                community</p>
-            </div>
-
+                    <h1><?php the_sub_field('title'); ?></h1>
+                    <p><?php the_sub_field('paragraph'); ?></p>
+                 </div>
+            <?php endwhile;?>
         </div>
         </article>
         </section>
+            <?php endif;?>
 
+        <?php 
+            $args = array(
+                'post_type' => 'profile',   
+                'post_status' => 'publish'
+                );
+            $acf_teams = get_field('meet_our_team');        
+            $featured_team = new WP_Query($args);
+
+
+        if(!empty($acf_teams) && (count($acf_teams) > 0)):
+            $ids = array();
+            foreach($acf_teams as $team):
+            $ids[] = $team->ID;
+            endforeach;
+            $featured_team = new WP_Query(array_merge($args, array('posts_per_page' => 5,'post__in' => $ids)));
+            else:
+            $featured_team = new WP_Query(array_merge($args, array('posts_per_page' => 5,'orderby' => 'rand','post_status' => 'publish')));
+            endif; 
+
+        ?>
+        <?php if($featured_team->have_posts()):?>
         <section class="section section-pair team-profile-container">
 
                 <h2 class="centered-title">Meet our team</h2>
             <div class="section-content section-content-plain np-team-profiles">
+                <?php while($featured_team->have_posts()): $featured_team->the_post();?>
                         <div class="section-content-item section-content-item-quarter profile">
                             <div class="image-wrap">
                                 <img src="../../wp-admin/images/avatar_placeholder_large.png" alt="">
                             </div>
-                            <h3 class="profile-name">Name</h3>
-                            <h5 class="profile-role">Role</h5>
-                            <h5 class="profile-role">Quote</h5>
-                            <a href="#">Link to full profile</a>
+                            <h3 class="profile-name"><?php the_title(); ?></h3>
+                            <h5 class="profile-role"><?php the_field('quote', $featured_leader->ID); ?></h5>
+                            <a href="<?php echo get_permalink();?>">Link to full profile</a>
                         </div> 
-                        <div class="section-content-item section-content-item-quarter profile">
-                            <div class="image-wrap">
-                                <img src="../../wp-admin/images/avatar_placeholder_large.png" alt="">
-                            </div>
-                            <h3 class="profile-name">Name</h3>
-                            <h5 class="profile-role">Role</h5>
-                            <h5 class="profile-role">Quote</h5>
-                            <a href="#">Link to full profile</a>
-                        </div> 
-                        <div class="section-content-item section-content-item-quarter profile">
-                            <div class="image-wrap">
-                                <img src="../../wp-admin/images/avatar_placeholder_large.png" alt="">
-                            </div>
-                            <h3 class="profile-name">Name</h3>
-                            <h5 class="profile-role">Role</h5>
-                            <h5 class="profile-role">Quote</h5>
-                            <a href="#">Link to full profile</a>
-                        </div> 
-                        <div class="section-content-item section-content-item-quarter profile">
-                            <div class="image-wrap">
-                                <img src="../../wp-admin/images/avatar_placeholder_large.png" alt="">
-                            </div>
-                            <h3 class="profile-name">Name</h3>
-                            <h5 class="profile-role">Role</h5>
-                            <h5 class="profile-role">Quote</h5>
-                            <a href="#">Link to full profile</a>
-                        </div> 
+                <?php endwhile;?>       
             </div>
         </section>
+        <?php wp_reset_postdata();?>
+        <?php endif;?>
 
 
+            <?php 
+            $url = get_field('apply');
+            ?>
                 <section class="section">
+                <?php if($url)?>
                     <div class="button-wrap">
-                        <a href="http://novaacademies.applytojob.com/" target="_blank" class="button button-wrap button-default button-primary" title="">APPLY NOW</a>
+                        <a href="<?php echo $url ?>" target="_blank" class="button button-wrap button-default button-primary" title="">APPLY NOW</a>
                     </div>
                 </section>
 
