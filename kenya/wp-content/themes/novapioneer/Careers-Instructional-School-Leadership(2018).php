0720115368e524077276d39849c0d1e597801e71
Young Mwanzia <young@circle.co.ke>
2018-04-09 16:44:30 +0300

    Just a test

--- kenya/wp-content/themes/novapioneer/Careers-Instructional-School-Leadership(2018).php
+++ kenya/wp-content/themes/novapioneer/Careers-Instructional-School-Leadership(2018).php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Template Name: Careers-Grow-with-us (2018)
+* Template Name: Careers-Teachers (2018)
 */
 
 get_header();?>
@@ -29,58 +29,66 @@ get_header();?>
         </section>
 
 
-        <!-- Intro -->
         <section class="section">
-            <?php //while(); ?>
-                <article class="article">
-                <p><?php echo the_content(); ?></p>
-                </article>
-            <?php //endwhile; wp_reset_postdata(); ?>
-        </section>
-        
-        <?php 
-            $body = 'body';
-        ?>
-
-        <section class="section ">
-            <?php if(have_rows($body)):?>
-            <div class="card-container">
-                <?php while(have_rows($body)): the_row();?>
-                <div class="card">
-                    <h2><?php the_sub_field('title') ?></h2>
-                    <p>
-                    <?php the_sub_field('paragraph') ?>
-                    </p>
-                </div>
-                <?php endwhile;?>
+            <div class="">
+                <p><?php the_content();?></p>
             </div>
-        <?php endif;?>
         </section>
 
-        <?php 
-            $school_leaders = 'why_become_nova_leader';
-            $title = 'title';
-            $paragraph = 'paragraph';
+
+        <?php
+            $body = 'body';
+
+            if(have_rows($body)):
         ?>
         <section class="section">
-            <article class="article">
-                <h3>Why become a school leader at Nova Pioneer?</h3> 
-            </article>
-            <?php if(have_rows($school_leaders)):?>
+            <div class="card-container steps-container">
+            <?php while(have_rows($body)): the_row()?>
+                <div class="card admission-step">
+                <h1><?php the_sub_field('title') ?></h1>
+                <p><?php the_sub_field('paragraph') ?>
+                </p>
 
-            <ol>
-                <?php while(have_rows($school_leaders)): the_row();?>
-                <li>
-                <b> <?php the_sub_field($title)?></b>
-                    <?php the_sub_field($paragraph) ?>
-                </li>
-
-            <? endwhile;?>
-
-            </ol>
-            
-            <? endif;?>
+                <blockquote><?php the_sub_field('quote') ?></blockquote>
+            </div>
+            <?php endwhile;?>
+            </div>
         </section>
+            <?php endif;?>
+
+
+        <?php
+            $app_program = 'teacher_apprentice_program';
+            if(have_rows($app_program)):
+        ?>
+
+        <section class="section">
+            <h1>Teacher Apprentice Program</h1>
+                
+            <div class="card-container" style="width:90%;">
+
+            <?php while(have_rows($app_program)): the_row(); ?>
+                <div class="card">
+                    <figure>
+                    <img style="width:100%;" src="<?php the_sub_field('icon')?>"></img>
+                    </figure>
+                </div>
+
+                <div class="card">
+                    <p><?php the_sub_field('paragraph');?>
+                    </p>
+                    <a href="<?php the_sub_field('link_url');?>">Learn more about the Teacher Apprentice Programme here</a>
+                </div>
+
+            <?php endwhile; ?> 
+
+            </div>
+                    <?php $url = get_field('apply_now');  if($url)?>
+                        <div class="button-wrap">
+                            <a href="<?php echo $url ?>" target="_blank" class="button button-wrap button-default button-primary" title="">APPLY NOW</a>
+                        </div>  
+                    <?php endif;?>    
+         </section>
 
 
         <?php
@@ -89,78 +97,62 @@ get_header();?>
                 'post_status' => 'publish',    
                 'tax_query' =>
                     array(
-                        'taxonomy' => 'leader'               
+                        'taxonomy' => 'teacher'               
                             )
                     
                 );
-            $acf_leaders = get_field('our_leaders');        
-            $featured_leader = new WP_Query($args);
+            $acf_teachers = get_field('meet_our_teacher');        
+            $featured_teacher = new WP_Query($args);
 
 
-        if(!empty($acf_leaders) && (count($acf_leaders) > 0)):
+        if(!empty($acf_teachers) && (count($acf_teachers) > 0)):
             $ids = array();
-            foreach($acf_leaders as $leader):
-            $ids[] = $leader->ID;
+            foreach($acf_teachers as $teacher):
+            $ids[] = $teacher->ID;
             endforeach;
-            $featured_leader = new WP_Query(array_merge($args, array('posts_per_page' => 5,'post__in' => $ids)));
+            $featured_teacher = new WP_Query(array_merge($args, array('posts_per_page' => 5,'post__in' => $ids)));
             else:
-            $featured_leader = new WP_Query(array_merge($args, array('posts_per_page' => 5,'orderby' => 'rand','post_status' => 'publish')));
+            $featured_teacher = new WP_Query(array_merge($args, array('posts_per_page' => 5,'orderby' => 'rand','post_status' => 'publish')));
             endif; 
-            
-
-            //var_dump($featured_leader);
         ?>
-        <?php if($featured_leader->have_posts() ): ?>
+        <?php if($featured_teacher->have_posts() ): ?>
         <section class="section section-pair team-profile-container">
 
-                <h2 class="centered-title">Meet our leaders</h2>
+                <h2 class="centered-title">Meet our teachers</h2>
             <div class="section-content section-content-plain np-team-profiles">
-            <?php while($featured_leader->have_posts()): $featured_leader->the_post(); ?>
+                <?php while($featured_teacher->have_posts()): $featured_teacher->the_post(); ?>
                         <div class="section-content-item section-content-item-quarter profile">
                             <div class="image-wrap">
                                 <img src="<?php if(has_post_thumbnail()) {echo get_the_post_thumbnail_url();}?>" alt="">
                             </div>
-                            <h3 class="profile-name"><?php the_title(); ?></h3>
-                           
-                            <h5 class="profile-role"><?php the_field('quote', $featured_leader->ID); the_field('quote', $featured_leader->ID); ?></h5>
+                            <h3 class="profile-name"><?php the_title();?></h3>
+                            <h5 class="profile-role"><?php the_field('quote', $featured_teacher->ID);?></h5>
                             <a href="<?php echo get_permalink();?>">Link to full profile</a>
                         </div> 
-            <?php endwhile; ?>
+                <?php endwhile; ?>
             </div>
         </section>
         <?php wp_reset_postdata();?>
-            <?php endif;?>
+        <?php endif;?>
 
-            <?php 
-            $_url = get_field('apply');
+       
+       <?php 
+                $field = 'faq';
+                $sub_field_1 = 'question';
+                $sub_field_2 = 'answer';
             ?>
-                <section class="section">
-                <?php if($_url):?>
-                    <div class="button-wrap">
-                        <a href="<?php echo $_url ?>" target="_blank" class="button button-wrap button-default button-primary" title="">APPLY NOW</a>
-                    </div>
-
-                <?php endif;?>
-                </section>
-
-
-            <?php 
-                $_field = 'faq';
-                $_sub_field_1 = 'question';
-                $_sub_field_2 = 'answer';
-            ?>
-            <section class="section">
+       <section class="section">
             <section class="faqs-container">
                  <article class="article article-inner article-inner-alt ">
                      <h2 id="faqs">Frequently Asked Questions</h2>
-                     <?php if(have_rows($_field)): $i = 0;?>
+                     <?php if(have_rows($field)): $i = 0;?>
 
                      <ul class="toggle-list">
-                            <?php while(have_rows($_field)):  the_row();?>
+                            <?php while(have_rows($field)):  the_row();?>
                                         <li class= '<?php if($i == 0){ echo 'show'; } ?>' >
-                                            <h3 class="toggle-list-title"><?php the_sub_field($_sub_field_1);?></h3>
+                                            <h3 class="toggle-list-title"><?php the_sub_field($sub_field_1);?></h3>
                                             <div class="toggle-list-content">
-                                            <?php the_sub_field($_sub_field_2);?>
+                                            <?php the_sub_field($sub_field_2);?>
                                             </div>
                                         </li>
                                 <?php $i += 1; ?>
@@ -171,7 +163,6 @@ get_header();?>
                  </article>
              </section>
              </section>
-
 
 
 
