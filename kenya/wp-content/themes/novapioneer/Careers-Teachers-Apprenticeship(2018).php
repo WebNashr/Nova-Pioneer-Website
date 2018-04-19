f9c89a9cd46b962b4da79f9734f137c696089cb0
Kumbi Madzinga <kumbi@circle.co.ke>
2018-04-19 16:18:09 +0300

    apprentice 1

--- kenya/wp-content/themes/novapioneer/Careers-Teachers-Apprenticeship(2018).php
+++ kenya/wp-content/themes/novapioneer/Careers-Teachers-Apprenticeship(2018).php
@@ -16,10 +16,8 @@ get_header();?>
                 </div>
             </div>
         </section>
-        
-        <section
-            class="section section-hero" <?php if (has_post_thumbnail()): echo 'style="background-image: url(' . wp_get_attachment_image_src(get_post_thumbnail_id(), 'single-post-thumbnail')[0] . ');"'; endif; ?>
-            data-enllax-ratio="0.1">
+
+        <section class="section section-hero" <?php if (has_post_thumbnail()): echo 'style="background-image: url(' . wp_get_attachment_image_src(get_post_thumbnail_id(), 'single-post-thumbnail')[0] . ');"'; endif; ?>>
             <div class="container hero-container">
                 <div class="main-callout-box">
                     <hr>
@@ -28,78 +26,116 @@ get_header();?>
             </div>
         </section>
 
-        
 
-        <section class="section">
+        <section class="section section-no-bottom section-page-intro">
             <article class="article">
-                <p><?php the_content(); ?></p>
+                <h2>Join us today</h2>
+                <?php the_content();?>
             </article>
         </section>
 
 
-        <?php 
+        <?php
             $body= 'body';
             if(have_rows($body)):
         ?>
-        <section class="section">
-        <h1>What the program offers</h1>
-            <div class="card-container steps-container">
-            <?php while(have_rows($body)): the_row()?>
-                <div class="card admission-step">
-                <h1><?php the_sub_field('title');?></h1>
-                <p><?php the_sub_field('paragraph');?>
-                </p>
-               </div>
-            <?php endwhile;?>   
+        <section class="section" style="display: none;">
+            <h1>What the program offers</h1>
 
-        </div>
+            <div class="card-container steps-container">
+                <?php while(have_rows($body)): the_row()?>
+                    <div class="card admission-step">
+                    <h1><?php the_sub_field('title');?></h1>
+                    <p><?php the_sub_field('paragraph');?>
+                    </p>
+                </div>
+                <?php endwhile;?>
+            </div>
+        </section>
+
+        <section class="section section-no-bottom">
+            <h2>What the program offers</h2>
+            <br>
+
+            <article class="article">
+                <div class="card-container-XXX steps-container-XXX new-card-container">
+                    <?php while(have_rows($body)): the_row();?>
+                        <div class="card-XXX admission-step-XXX new-card-item new-card-item-third">
+                            <figure class="new-card-img">
+                                <img src="https://placeimg.com/480/320/people" alt=""><!--!!! Young: this needs an image -->
+                            </figure>
+
+                            <div class="new-card-copy">
+                                <!--<h2><?php the_sub_field('title'); ?></h2>-->
+                                <h3><?php the_sub_field('title'); ?></h3>
+                                <p><?php the_sub_field('paragraph'); ?></p>
+                            </div>
+                        </div>
+                    <?php endwhile?>
+                </div>
+            </article>
         </section>
         <?php endif;?>
 
-        <?php 
+
+        <?php
             $who_may_apply = 'who_may_apply';
             if(have_rows($who_may_apply)):
         ?>
-        <section class="section">
-                <h1>Who may apply?</h1>
-                <p>You may apply if you:</p>   
+        <section class="section" style="display: none;">
+            <h1>Who may apply?</h1>
+            <p>You may apply if you:</p>
+
             <div class="card-container steps-container">
+                <?php while(have_rows($who_may_apply)): the_row()?>
+                    <div class="card admission-step">
+                        <h1><?php the_sub_field('title');?></h1>
+                        <p><?php the_sub_field('paragraph');?></p>
+                    </div>
+                <?php endwhile;?>
+            </div>
+        </section>
 
-            <?php while(have_rows($who_may_apply)): the_row()?>  
-                <div class="card admission-step">
-                    <h1><?php the_sub_field('title');?></h1>
-                    <p><?php the_sub_field('paragraph');?></p>
+        <section class="section section-no-bottom">
+            <h2>Who may apply?</h2>
+            <p>You may apply if you:</p>
+            <br>
+
+            <article class="article">
+                <div class="container">
+                    <ul>
+                    <?php while(have_rows($who_may_apply)): the_row()?>
+                        <li><?php the_sub_field('paragraph');?></li>
+                    <?php endwhile;?>
+                    </ul>
                 </div>
-            <?php endwhile;?>   
-
-             </div>
+            </article>
         </section>
         <?php endif;?>
 
-        </section>
 
-            <?php 
+        <?php
             $url = get_field('apply');
-            ?>
-                <section class="section">
-                <?php if($url)?>
-                    <div class="button-wrap">
-                        <a href="<?php echo $url ?>" target="_blank" class="button button-wrap button-default button-primary" title="">APPLY TO BE AN APPRENTICE NOW</a>
-                    </div>
-                </section>
+        ?>
+        <section class="section">
+            <?php if($url)?>
+            <div class="button-wrap">
+                <a href="<?php echo $url ?>" target="_blank" class="button button-wrap button-default button-primary" title="">APPLY TO BE AN APPRENTICE NOW</a>
+            </div>
+        </section>
 
 
         <?php
             $args = array(
-                'post_type' => 'profile',   
-                'post_status' => 'publish',    
+                'post_type' => 'profile',
+                'post_status' => 'publish',
                 'tax_query' =>
                     array(
-                        'taxonomy' => 'apprentice'               
+                        'taxonomy' => 'apprentice'
                             )
-                    
+
                 );
-            $acf_teachers = get_field('meet_our_teachers');        
+            $acf_teachers = get_field('meet_our_teachers');
             $featured_teacher = new WP_Query($args);
 
 
@@ -111,7 +147,7 @@ get_header();?>
             $featured_teacher = new WP_Query(array_merge($args, array('posts_per_page' => 5,'post__in' => $ids)));
             else:
             $featured_teacher = new WP_Query(array_merge($args, array('posts_per_page' => 5,'orderby' => 'rand','post_status' => 'publish')));
-            endif; 
+            endif;
         ?>
         <?php if($featured_teacher->have_posts() ): ?>
         <section class="section section-pair team-profile-container">
@@ -126,26 +162,26 @@ get_header();?>
                             <h3 class="profile-name"><?php the_title();?></h3>
                             <h5 class="profile-role"><?php the_field('quote', $featured_teacher->ID);?></h5>
                             <a href="<?php echo get_permalink();?>">Link to full profile</a>
-                        </div> 
+                        </div>
                 <?php endwhile; ?>
             </div>
         </section>
         <?php wp_reset_postdata();?>
         <?php endif;?>
 
-       
-       <?php 
-                $field = 'faq';
-                $sub_field_1 = 'question';
-                $sub_field_2 = 'answer';
-            ?>
-       <section class="section">
-            <section class="faqs-container">
-                 <article class="article article-inner article-inner-alt ">
-                     <h2 id="faqs">Frequently Asked Questions</h2>
-                     <?php if(have_rows($field)): $i = 0;?>
 
-                     <ul class="toggle-list">
+        <?php
+            $field = 'faq';
+            $sub_field_1 = 'question';
+            $sub_field_2 = 'answer';
+        ?>
+        <section class="section">
+            <section class="faqs-container">
+                <article class="article article-inner article-inner-alt ">
+                    <h2 id="faqs">Frequently Asked Questions</h2>
+                    <?php if(have_rows($field)): $i = 0;?>
+
+                    <ul class="toggle-list">
                             <?php while(have_rows($field)):  the_row();?>
                                         <li class= '<?php if($i == 0){ echo 'show'; } ?>' >
                                             <h3 class="toggle-list-title"><?php the_sub_field($sub_field_1);?></h3>
@@ -155,14 +191,14 @@ get_header();?>
                                         </li>
                                 <?php $i += 1; ?>
                             <?php  endwhile; wp_reset_postdata();?>
-                     </ul>
+                    </ul>
 
                 <?php endif; ?>
-                 </article>
-             </section>
-             </section>
+                </article>
+            </section>
+        </section>
 
-       
+
 
 
 
