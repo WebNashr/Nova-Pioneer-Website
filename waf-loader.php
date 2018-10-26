4ce220f67f430ab16b56420c0edeb5908a3d4175
edgar chris <edgarchris4@gmail.com>
2018-10-26 18:55:07 +0300

    add a waf loader

---
+++ waf-loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: edgarchris
+ * Date: 10/26/18
+ * Time: 18:53
+ */
+
+require '/var/www/html/nova/kenya/wordfence-waf.php';
+require '/var/www/html/nova/sa/wordfence-waf.php';
+require '/var/www/html/nova/careers/wordfence-waf.php';
