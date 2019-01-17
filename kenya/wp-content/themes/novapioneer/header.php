5c5c9f94a91a904f8cb0950ab069fad8aa4bc80f
edgar chris <edgarchris4@gmail.com>
2019-01-17 12:10:44 +0300

    kill tracking code as suggested by nova

--- kenya/wp-content/themes/novapioneer/header.php
+++ kenya/wp-content/themes/novapioneer/header.php
@@ -26,45 +26,30 @@
     <!-- End Google Tag Manager -->
 
 
-    <?php if (novap_ga_tracking_id()): ?>
-        <!-- Google Analytics -->
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+<!--    --><?php //if (novap_ga_tracking_id()): ?>
+<!--   -->
+<!---->
+<!--    --><?php //endif; ?>
+    <!-- Google Analytics -->
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-            ga('create', '<?php echo novap_ga_tracking_id() ?>', 'auto');
-            ga('send', 'pageview');
-        </script>
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-97642297-1', 'auto');
+        ga('send', 'pageview');
+        gtag('config', 'AW-852162782');
 
-            ga('create', 'UA-97642297-1', 'auto');
-            ga('send', 'pageview');
-            gtag('config', 'AW-852162782');
-
-        </script>
-        <!-- End Google Analytics -->
-
-    <?php endif; ?>
+    </script>
+    <!-- End Google Analytics -->
     <script type="text/javascript">
         /* <![CDATA[ */
         var google_conversion_id = 966928664;
--- sa/wp-content/themes/novapioneer/header.php
+++ sa/wp-content/themes/novapioneer/header.php
@@ -26,26 +26,8 @@
     <!-- End Google Tag Manager -->
 
 
-    <?php if (novap_ga_tracking_id()): ?>
-        <!-- Google Analytics -->
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', '<?php echo novap_ga_tracking_id() ?>', 'auto');
-            ga('send', 'pageview');
-        </script>
-        <!-- End Google Analytics -->
-    <?php endif; ?>
+<!--    --><?php //if (novap_ga_tracking_id()): ?>
+<!--    --><?php //endif; ?>
     <script>
         (function (i, s, o, g, r, a, m) {
             i['GoogleAnalyticsObject'] = r;
