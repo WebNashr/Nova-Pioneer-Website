4296ad20754d596654ff913fcf52242869175b32
Kumbi Madzinga <kumbi@circle.co.ke>
2019-04-02 11:10:49 +0300

    #update: added some admissions styles

--- kenya/wp-content/themes/novapioneer/admissions-process.php
+++ kenya/wp-content/themes/novapioneer/admissions-process.php
@@ -8,7 +8,7 @@
 
 <?php if( have_posts() ): ?>
 <?php while( have_posts() ): the_post(); ?>
-<div class="wrapper">
+<div class="updates-2019">
     <!--<section class="section section-hero">
         <img src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($school->ID), '16-9-large')[0]; ?>" alt="">
 
@@ -29,44 +29,77 @@
 
     <!--<div class="trigger"></div>-->
 
-    <section class="section section-admissions">
+    <section class="section section-admissions section-admissions-large">
         <div class="card-container">
-            <figure class="card card-admissions">
-                <h4>Admissions process</h4>
-                <p>kashg fjw vkwrkvu gwrvkwbkvb kwvk gwiu bwkv wiug bwe jbkwjkb og ougb wb</p>
+            <figure class="card card-admissions card-admissions-large">
+                <h4><?php the_field('admissions_title'); ?></h4>
+                <br>
+                <p><?php the_field('admissions_description'); ?></p>
             </figure>
 
-            <?php $step_number = 0; ?>
-            <?php foreach( get_field('steps') as $step ): $step = (object)$step; $step_number++; ?>
-                <figure class="card card-admissions">
-                    <img
-                        class="school-photo"
-                        src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($step->image), '16-9-tall')[0]; ?>"
-                        alt="<?php echo $step->title; ?>"
-                    >
+            <?php if( have_rows('steps') ): ?>
+                <?php while( have_rows('steps') ): the_row();  ?>
+                <figure class="card card-admissions card-admissions-large">
+                    <?php
+                        $image = get_sub_field('image');
+                        $size = '16-9-tall';
+                        if( $image ) {
+                        echo wp_get_attachment_image( $image, $size );
+                        }
+                    ?>
+
                     <figcaption>
-                        <div class="step-number"><?php echo $step_number; ?></div>
-                        <h4><?php echo $step->title; ?></h4>
-                        <?php echo $step->description; ?>
+                        <div class="admissions-number"><?php the_sub_field('number'); ?></div>
+                        <p class="admissions-title"><?php the_sub_field('title'); ?></p>
+                        <div class="admissions-description"><?php the_sub_field('description'); ?></div>
                     </figcaption>
                 </figure>
-            <?php endforeach; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </section>
 
-    <section class="section section-divider"></section>
+    <div class="divider-rose">
+        <svg xmlns="http://www.w3.org/2000/svg" width="195" height="46.819" viewBox="0 0 195 46.819">
+            <g id="divider" transform="translate(0 -0.359)">
+                <g id="Group_4_Copy_6" data-name="Group 4 Copy 6">
+                <g id="logo-mark-white_copy_3" data-name="logo-mark-white copy 3" transform="translate(75)">
+                    <g id="white" transform="translate(0.59 0.36)">
+                    <path id="Shape" d="M18.628,46.819,5.246,38.949,0,33V16.943L2.726,9.58,16.7,1.514,24.467,0,38.145,8.072l5.143,6.149V30.168l-2.923,7.566L26.4,45.508l-7.77,1.311ZM9.9,18.325,6.373,20.351V38.017l12.52,7.365,5.3-.9-1.264-.75L9.9,36.014V18.325Zm1.375,12.64V35.23l12.282,7.278,2.615,1.549,13.088-7.285,2.01-5.2L27.217,39.4l-.336.187h-.005L26.2,39.58ZM38.386,10.5V27.484L32.8,30.375l-9.9,5.713L26.553,38.2l15.36-8.556V14.722ZM16.636,8.64h0L1.375,17.465V32.478L5,36.59V19.561l5.25-3.019L20.185,10.8,16.636,8.64Zm4.9,2.974h0L11.277,17.535V29.378L21.53,35.3l10.255-5.92V17.535L21.532,11.614ZM24.21,1.451,18.851,2.5l14.309,8.39V28.642l3.851-1.994V9Zm-7.555,5.59h0l4.7,2.866.527.321,9.909,5.722.025-4.257L16.932,2.964,3.842,10.522l-1.849,5L16.655,7.042Z" fill="#9b9b9b"/>
+                    </g>
+                </g>
+                <rect id="Rectangle_3" data-name="Rectangle 3" width="48" height="1" transform="translate(147 24)" fill="#9b9b9b"/>
+                <rect id="Rectangle_3_Copy" data-name="Rectangle 3 Copy" width="48" height="1" transform="translate(0 24)" fill="#9b9b9b"/>
+                </g>
+            </g>
+        </svg>
+    </div>
 
-    <section class="section section-centered">
+    <section class="section section-signpost">
         <h2>Start your journey with Nova Pioneer today</h2>
-        <p>bdak bkjvsjv bkjsbkjsbkj bksj dbvjsvj bosub jb o u hwe bvjwevouw</p>
-        <a href="" class="button button-large">Apply now</a>
+        <p>You and your child will be invited for an interview and assessment. The aim of this assessment is to get insight into the student as an individual in order to determine how we can best support and develop your child at Nova Pioneer.</p>
+        <a class="button button-large button-raspberry" href="" title="">Apply now</a>
     </section>
 
-    <section class="section section-divider"></section>
+    <div class="divider-rose">
+        <svg xmlns="http://www.w3.org/2000/svg" width="195" height="46.819" viewBox="0 0 195 46.819">
+            <g id="divider" transform="translate(0 -0.359)">
+                <g id="Group_4_Copy_6" data-name="Group 4 Copy 6">
+                <g id="logo-mark-white_copy_3" data-name="logo-mark-white copy 3" transform="translate(75)">
+                    <g id="white" transform="translate(0.59 0.36)">
+                    <path id="Shape" d="M18.628,46.819,5.246,38.949,0,33V16.943L2.726,9.58,16.7,1.514,24.467,0,38.145,8.072l5.143,6.149V30.168l-2.923,7.566L26.4,45.508l-7.77,1.311ZM9.9,18.325,6.373,20.351V38.017l12.52,7.365,5.3-.9-1.264-.75L9.9,36.014V18.325Zm1.375,12.64V35.23l12.282,7.278,2.615,1.549,13.088-7.285,2.01-5.2L27.217,39.4l-.336.187h-.005L26.2,39.58ZM38.386,10.5V27.484L32.8,30.375l-9.9,5.713L26.553,38.2l15.36-8.556V14.722ZM16.636,8.64h0L1.375,17.465V32.478L5,36.59V19.561l5.25-3.019L20.185,10.8,16.636,8.64Zm4.9,2.974h0L11.277,17.535V29.378L21.53,35.3l10.255-5.92V17.535L21.532,11.614ZM24.21,1.451,18.851,2.5l14.309,8.39V28.642l3.851-1.994V9Zm-7.555,5.59h0l4.7,2.866.527.321,9.909,5.722.025-4.257L16.932,2.964,3.842,10.522l-1.849,5L16.655,7.042Z" fill="#9b9b9b"/>
+                    </g>
+                </g>
+                <rect id="Rectangle_3" data-name="Rectangle 3" width="48" height="1" transform="translate(147 24)" fill="#9b9b9b"/>
+                <rect id="Rectangle_3_Copy" data-name="Rectangle 3 Copy" width="48" height="1" transform="translate(0 24)" fill="#9b9b9b"/>
+                </g>
+            </g>
+        </svg>
+    </div>
 
     <section class="section section-faq faqs-container">
         <article class="article article-inner article-inner-alt ">
-            <h2 id="faqs">Frequently Asked Questions</h2>
+            <h2 id="xxx-faqs">Frequently Asked Questions</h2>
 
             <ul class="toggle-list">
                 <?php $n = 0; ?>
@@ -84,7 +117,21 @@
         </article>
     </section>
 
-    <section class="section section-divider"></section>
+    <div class="divider-rose">
+        <svg xmlns="http://www.w3.org/2000/svg" width="195" height="46.819" viewBox="0 0 195 46.819">
+            <g id="divider" transform="translate(0 -0.359)">
+                <g id="Group_4_Copy_6" data-name="Group 4 Copy 6">
+                <g id="logo-mark-white_copy_3" data-name="logo-mark-white copy 3" transform="translate(75)">
+                    <g id="white" transform="translate(0.59 0.36)">
+                    <path id="Shape" d="M18.628,46.819,5.246,38.949,0,33V16.943L2.726,9.58,16.7,1.514,24.467,0,38.145,8.072l5.143,6.149V30.168l-2.923,7.566L26.4,45.508l-7.77,1.311ZM9.9,18.325,6.373,20.351V38.017l12.52,7.365,5.3-.9-1.264-.75L9.9,36.014V18.325Zm1.375,12.64V35.23l12.282,7.278,2.615,1.549,13.088-7.285,2.01-5.2L27.217,39.4l-.336.187h-.005L26.2,39.58ZM38.386,10.5V27.484L32.8,30.375l-9.9,5.713L26.553,38.2l15.36-8.556V14.722ZM16.636,8.64h0L1.375,17.465V32.478L5,36.59V19.561l5.25-3.019L20.185,10.8,16.636,8.64Zm4.9,2.974h0L11.277,17.535V29.378L21.53,35.3l10.255-5.92V17.535L21.532,11.614ZM24.21,1.451,18.851,2.5l14.309,8.39V28.642l3.851-1.994V9Zm-7.555,5.59h0l4.7,2.866.527.321,9.909,5.722.025-4.257L16.932,2.964,3.842,10.522l-1.849,5L16.655,7.042Z" fill="#9b9b9b"/>
+                    </g>
+                </g>
+                <rect id="Rectangle_3" data-name="Rectangle 3" width="48" height="1" transform="translate(147 24)" fill="#9b9b9b"/>
+                <rect id="Rectangle_3_Copy" data-name="Rectangle 3 Copy" width="48" height="1" transform="translate(0 24)" fill="#9b9b9b"/>
+                </g>
+            </g>
+        </svg>
+    </div>
 </div>
 <?php endwhile; ?>
 <?php endif; ?>
