5cf5d6e708940f04089438e5b105dfe78c64f24e
Kumbi Madzinga <kumbi@circle.co.ke>
2019-05-06 10:36:43 +0300

    #fix: school hover button; testimonial move; team spacing/size

--- sa/wp-content/themes/novapioneer/2019-school-home-page.php
+++ sa/wp-content/themes/novapioneer/2019-school-home-page.php
@@ -698,6 +698,42 @@ get_header(); ?>
     </div>
     
 
+    <section class="section section-school-tesimonials">
+        <?php foreach (get_field('testimonials') as $testimonial): $testimonial = (object)$testimonial; ?>
+        <div class="section-school-testimonials-item">
+            <img src="<?php echo get_the_post_thumbnail_url($testimonial->ID, '1-1-square'); ?>">
+
+            <div class="testimonial-text">
+                <blockquote><?php echo $testimonial->post_content; ?></blockquote>
+
+                <cite>
+                    <span class="cite-by"><?php echo get_field('reviewer_name', $testimonial->ID); ?></span>, 
+                    <span class="cite-role"><?php echo get_field('reviewer_title', $testimonial->ID); ?></span>
+                </cite>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </section>
+
+
+    <div class="divider-rose">
+        <svg xmlns="http://www.w3.org/2000/svg" width="195" height="46.819" viewBox="0 0 195 46.819">
+            <g id="divider" transform="translate(0 -0.359)">
+                <g id="Group_4_Copy_6" data-name="Group 4 Copy 6">
+                <g id="logo-mark-white_copy_3" data-name="logo-mark-white copy 3" transform="translate(75)">
+                    <g id="white" transform="translate(0.59 0.36)">
+                    <path id="Shape" d="M18.628,46.819,5.246,38.949,0,33V16.943L2.726,9.58,16.7,1.514,24.467,0,38.145,8.072l5.143,6.149V30.168l-2.923,7.566L26.4,45.508l-7.77,1.311ZM9.9,18.325,6.373,20.351V38.017l12.52,7.365,5.3-.9-1.264-.75L9.9,36.014V18.325Zm1.375,12.64V35.23l12.282,7.278,2.615,1.549,13.088-7.285,2.01-5.2L27.217,39.4l-.336.187h-.005L26.2,39.58ZM38.386,10.5V27.484L32.8,30.375l-9.9,5.713L26.553,38.2l15.36-8.556V14.722ZM16.636,8.64h0L1.375,17.465V32.478L5,36.59V19.561l5.25-3.019L20.185,10.8,16.636,8.64Zm4.9,2.974h0L11.277,17.535V29.378L21.53,35.3l10.255-5.92V17.535L21.532,11.614ZM24.21,1.451,18.851,2.5l14.309,8.39V28.642l3.851-1.994V9Zm-7.555,5.59h0l4.7,2.866.527.321,9.909,5.722.025-4.257L16.932,2.964,3.842,10.522l-1.849,5L16.655,7.042Z" fill="#9b9b9b"/>
+                    </g>
+                </g>
+                <rect id="Rectangle_3" data-name="Rectangle 3" width="48" height="1" transform="translate(147 24)" fill="#9b9b9b"/>
+                <rect id="Rectangle_3_Copy" data-name="Rectangle 3 Copy" width="48" height="1" transform="translate(0 24)" fill="#9b9b9b"/>
+                </g>
+            </g>
+        </svg>
+    </div>
+
+
+
     <section class="section section-school-gallery">
         <h2 class="section-heading"><?php the_field('school_gallery_title'); ?></h2>
 
@@ -846,41 +882,6 @@ get_header(); ?>
                 novap_render_google_map($locations);
             } ?>
         </div>
-    </section>
-
-
-    <div class="divider-rose">
-        <svg xmlns="http://www.w3.org/2000/svg" width="195" height="46.819" viewBox="0 0 195 46.819">
-            <g id="divider" transform="translate(0 -0.359)">
-                <g id="Group_4_Copy_6" data-name="Group 4 Copy 6">
-                <g id="logo-mark-white_copy_3" data-name="logo-mark-white copy 3" transform="translate(75)">
-                    <g id="white" transform="translate(0.59 0.36)">
-                    <path id="Shape" d="M18.628,46.819,5.246,38.949,0,33V16.943L2.726,9.58,16.7,1.514,24.467,0,38.145,8.072l5.143,6.149V30.168l-2.923,7.566L26.4,45.508l-7.77,1.311ZM9.9,18.325,6.373,20.351V38.017l12.52,7.365,5.3-.9-1.264-.75L9.9,36.014V18.325Zm1.375,12.64V35.23l12.282,7.278,2.615,1.549,13.088-7.285,2.01-5.2L27.217,39.4l-.336.187h-.005L26.2,39.58ZM38.386,10.5V27.484L32.8,30.375l-9.9,5.713L26.553,38.2l15.36-8.556V14.722ZM16.636,8.64h0L1.375,17.465V32.478L5,36.59V19.561l5.25-3.019L20.185,10.8,16.636,8.64Zm4.9,2.974h0L11.277,17.535V29.378L21.53,35.3l10.255-5.92V17.535L21.532,11.614ZM24.21,1.451,18.851,2.5l14.309,8.39V28.642l3.851-1.994V9Zm-7.555,5.59h0l4.7,2.866.527.321,9.909,5.722.025-4.257L16.932,2.964,3.842,10.522l-1.849,5L16.655,7.042Z" fill="#9b9b9b"/>
-                    </g>
-                </g>
-                <rect id="Rectangle_3" data-name="Rectangle 3" width="48" height="1" transform="translate(147 24)" fill="#9b9b9b"/>
-                <rect id="Rectangle_3_Copy" data-name="Rectangle 3 Copy" width="48" height="1" transform="translate(0 24)" fill="#9b9b9b"/>
-                </g>
-            </g>
-        </svg>
-    </div>
-    
-
-    <section class="section section-school-tesimonials">
-        <?php foreach (get_field('testimonials') as $testimonial): $testimonial = (object)$testimonial; ?>
-        <div class="section-school-testimonials-item">
-            <img src="<?php echo get_the_post_thumbnail_url($testimonial->ID, '1-1-square'); ?>">
-
-            <div class="testimonial-text">
-                <blockquote><?php echo $testimonial->post_content; ?></blockquote>
-
-                <cite>
-                    <span class="cite-by"><?php echo get_field('reviewer_name', $testimonial->ID); ?></span>, 
-                    <span class="cite-role"><?php echo get_field('reviewer_title', $testimonial->ID); ?></span>
-                </cite>
-            </div>
-        </div>
-        <?php endforeach; ?>
     </section>
 
 
